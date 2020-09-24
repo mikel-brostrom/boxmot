4af7984d5002c050588286602e87b07ed79463ca
Jirka Borovec <jirka.borovec@seznam.cz>
2020-09-24 13:42:20 +0200

    formatting

--- track.py
+++ track.py
@@ -22,7 +22,6 @@ from yolov5.utils.general import check_img_size, non_max_suppression, scale_coor
 from yolov5.utils.torch_utils import select_device, time_synchronized
 
 
-
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
 
 
@@ -73,11 +72,17 @@ def detect(opt, save_img=False):
     # initialize deepsort
     cfg = get_config()
     cfg.merge_from_file(opt.config_deepsort)
-    deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
-                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
-                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
-                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
-                        use_cuda=True)
+    deepsort = DeepSort(
+        cfg.DEEPSORT.REID_CKPT,
+        max_dist=cfg.DEEPSORT.MAX_DIST,
+        min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP,
+        max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+        max_age=cfg.DEEPSORT.MAX_AGE,
+        n_init=cfg.DEEPSORT.N_INIT,
+        nn_budget=cfg.DEEPSORT.NN_BUDGET,
+        use_cuda=True,
+    )
 
     # Initialize
     device = select_device(opt.device)
@@ -234,7 +239,7 @@ if __name__ == '__main__':
     parser.add_argument("--config_deepsort", type=str, default="deep_sort/configs/deep_sort.yaml")
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
-    print(args)
+    pprint(vars(args))
 
     with torch.no_grad():
         detect(args)
