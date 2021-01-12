5bcdda153a925dd0316d2dccd5e7e50dcfb88ac3
mikel.brostrom <mikel.brostrom@cevt.se>
2021-01-12 11:16:36 +0100

    class_id stored in track

--- deep_sort_pytorch/deep_sort/sort/track.py
+++ deep_sort_pytorch/deep_sort/sort/track.py
@@ -63,11 +63,12 @@ class Track:
 
     """
 
-    def __init__(self, mean, covariance, track_id, n_init, max_age,
+    def __init__(self, mean, covariance, track_id, class_id, n_init, max_age,
                  feature=None):
         self.mean = mean
         self.covariance = covariance
         self.track_id = track_id
+        self.class_id = class_id
         self.hits = 1
         self.age = 1
         self.time_since_update = 0
