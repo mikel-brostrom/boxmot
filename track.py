88c1a7d31da058045f3e70dd2839a4d2a3d35031
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-06-06 10:42:50 +0200

    tracker arguments clean-up

--- track.py
+++ track.py
@@ -241,40 +241,25 @@ def detect(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str,
-                        default='yolov5/weights/crowdhuman_yolov5m.pt', help='model.pt path')
+    parser.add_argument('--weights', type=str, default='yolov5/weights/yolov5s.pt', help='model.pt path')
     # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str,
-                        default='inference/images', help='source')
-    parser.add_argument('--output', type=str, default='inference/output',
-                        help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=640,
-                        help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.4, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v',
-                        help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-vid', action='store_false',
-                        help='display results')
-    parser.add_argument('--save-vid', action='store_true',
-                        help='display results')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save results to *.txt')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-vid', action='store_false', help='display results')
+    parser.add_argument('--save-vid', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     # class 0 is person
     parser.add_argument('--classes', nargs='+', default=[0], type=int, help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument("--config_deepsort", type=str,
-                        default="deep_sort_pytorch/configs/deep_sort.yaml")
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument("--config_deepsort", type=str, default="deep_sort_pytorch/configs/deep_sort.yaml")
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
-    print(args)
 
     with torch.no_grad():
         detect(args)
