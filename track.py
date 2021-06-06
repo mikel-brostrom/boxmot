d38a0920df466dfdd222b0dc2989bc58e05045aa
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-06-06 10:47:18 +0200

    tracker arguments clarification

--- track.py
+++ track.py
@@ -246,11 +246,11 @@ if __name__ == '__main__':
     parser.add_argument('--source', type=str, default='inference/images', help='source')
     parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-vid', action='store_false', help='display results')
+    parser.add_argument('--view-vid', action='store_true', help='display results')
     parser.add_argument('--save-vid', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     # class 0 is person
