4af3111ac61f1f8e4698c08b201498060daa48f6
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-09-14 16:43:34 +0200

    make it possible to choose between yolo bboxes and kf predicted bboxes

--- track.py
+++ track.py
@@ -33,7 +33,7 @@ def detect(opt):
     attempt_download(deep_sort_weights, repo='mikel-brostrom/Yolov5_DeepSort_Pytorch')
     deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
                         max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
-                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                         max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
                         use_cuda=True)
 
@@ -125,7 +125,7 @@ def detect(opt):
                 clss = det[:, 5]
 
                 # pass detections to deepsort
-                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0, 'kf')
                 
                 # draw boxes for visualization
                 if len(outputs) > 0:
