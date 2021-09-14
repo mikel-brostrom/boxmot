6da9f132e5a343b732fe8d4d2420c8e9ed232392
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-09-14 16:42:32 +0200

    make it possible to choose between yolo bboxes and kf predicted bboxes

--- deep_sort_pytorch/deep_sort/deep_sort.py
+++ deep_sort_pytorch/deep_sort/deep_sort.py
@@ -11,9 +11,8 @@ __all__ = ['DeepSort']
 
 
 class DeepSort(object):
-    def __init__(self, model_path, max_dist=0.2, min_confidence=0.3, nms_max_overlap=1.0, max_iou_distance=0.7, max_age=70, n_init=3, nn_budget=100, use_cuda=True):
+    def __init__(self, model_path, max_dist=0.2, min_confidence=0.3, max_iou_distance=0.7, max_age=70, n_init=3, nn_budget=100, use_cuda=True):
         self.min_confidence = min_confidence
-        self.nms_max_overlap = nms_max_overlap
 
         self.extractor = Extractor(model_path, use_cuda=use_cuda)
 
@@ -23,7 +22,7 @@ class DeepSort(object):
         self.tracker = Tracker(
             metric, max_iou_distance=max_iou_distance, max_age=max_age, n_init=n_init)
 
-    def update(self, bbox_xywh, confidences, classes, ori_img):
+    def update(self, bbox_xywh, confidences, classes, ori_img, choose_bbox='yolo'):
         self.height, self.width = ori_img.shape[:2]
         # generate detections
         features = self._get_features(bbox_xywh, ori_img)
@@ -44,7 +43,10 @@ class DeepSort(object):
         for track in self.tracker.tracks:
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue
-            box = track.to_tlwh()
+            if choose_bbox == 'yolo':
+                box = track.get_yolo_pred()
+            elif choose_bbox == 'kf':
+                box = track.to_tlwh()
             x1, y1, x2, y2 = self._tlwh_to_xyxy(box)
             track_id = track.track_id
             class_id = track.class_id
