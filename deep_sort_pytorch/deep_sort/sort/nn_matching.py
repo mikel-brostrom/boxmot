774c7ad372582b2a5f62a7247f14846fdb8c029a
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-12-03 20:02:56 +0100

    .

--- deep_sort_pytorch/deep_sort/sort/nn_matching.py
+++ deep_sort_pytorch/deep_sort/sort/nn_matching.py
@@ -4,20 +4,17 @@ import numpy as np
 
 def _pdist(a, b):
     """Compute pair-wise squared distance between points in `a` and `b`.
-
     Parameters
     ----------
     a : array_like
         An NxM matrix of N samples of dimensionality M.
     b : array_like
         An LxM matrix of L samples of dimensionality M.
-
     Returns
     -------
     ndarray
         Returns a matrix of size len(a), len(b) such that eleement (i, j)
         contains the squared distance between `a[i]` and `b[j]`.
-
     """
     a, b = np.asarray(a), np.asarray(b)
     if len(a) == 0 or len(b) == 0:
@@ -30,7 +27,6 @@ def _pdist(a, b):
 
 def _cosine_distance(a, b, data_is_normalized=False):
     """Compute pair-wise cosine distance between points in `a` and `b`.
-
     Parameters
     ----------
     a : array_like
@@ -40,13 +36,11 @@ def _cosine_distance(a, b, data_is_normalized=False):
     data_is_normalized : Optional[bool]
         If True, assumes rows in a and b are unit length vectors.
         Otherwise, a and b are explicitly normalized to lenght 1.
-
     Returns
     -------
     ndarray
         Returns a matrix of size len(a), len(b) such that eleement (i, j)
         contains the squared distance between `a[i]` and `b[j]`.
-
     """
     if not data_is_normalized:
         a = np.asarray(a) / np.linalg.norm(a, axis=1, keepdims=True)
@@ -56,20 +50,17 @@ def _cosine_distance(a, b, data_is_normalized=False):
 
 def _nn_euclidean_distance(x, y):
     """ Helper function for nearest neighbor distance metric (Euclidean).
-
     Parameters
     ----------
     x : ndarray
         A matrix of N row-vectors (sample points).
     y : ndarray
         A matrix of M row-vectors (query points).
-
     Returns
     -------
     ndarray
         A vector of length M that contains for each entry in `y` the
         smallest Euclidean distance to a sample in `x`.
-
     """
     distances = _pdist(x, y)
     return np.maximum(0.0, distances.min(axis=0))
@@ -77,20 +68,17 @@ def _nn_euclidean_distance(x, y):
 
 def _nn_cosine_distance(x, y):
     """ Helper function for nearest neighbor distance metric (cosine).
-
     Parameters
     ----------
     x : ndarray
         A matrix of N row-vectors (sample points).
     y : ndarray
         A matrix of M row-vectors (query points).
-
     Returns
     -------
     ndarray
         A vector of length M that contains for each entry in `y` the
         smallest cosine distance to a sample in `x`.
-
     """
     distances = _cosine_distance(x, y)
     return distances.min(axis=0)
@@ -100,7 +88,6 @@ class NearestNeighborDistanceMetric(object):
     """
     A nearest neighbor distance metric that, for each target, returns
     the closest distance to any sample that has been observed so far.
-
     Parameters
     ----------
     metric : str
@@ -111,17 +98,14 @@ class NearestNeighborDistanceMetric(object):
     budget : Optional[int]
         If not None, fix samples per class to at most this number. Removes
         the oldest samples when the budget is reached.
-
     Attributes
     ----------
     samples : Dict[int -> List[ndarray]]
         A dictionary that maps from target identities to the list of samples
         that have been observed so far.
-
     """
 
     def __init__(self, metric, matching_threshold, budget=None):
-
         if metric == "euclidean":
             self._metric = _nn_euclidean_distance
         elif metric == "cosine":
@@ -135,7 +119,6 @@ class NearestNeighborDistanceMetric(object):
 
     def partial_fit(self, features, targets, active_targets):
         """Update the distance metric with new data.
-
         Parameters
         ----------
         features : ndarray
@@ -144,7 +127,6 @@ class NearestNeighborDistanceMetric(object):
             An integer array of associated target identities.
         active_targets : List[int]
             A list of targets that are currently present in the scene.
-
         """
         for feature, target in zip(features, targets):
             self.samples.setdefault(target, []).append(feature)
@@ -154,21 +136,18 @@ class NearestNeighborDistanceMetric(object):
 
     def distance(self, features, targets):
         """Compute distance between features and targets.
-
         Parameters
         ----------
         features : ndarray
             An NxM matrix of N features of dimensionality M.
         targets : List[int]
             A list of targets to match the given `features` against.
-
         Returns
         -------
         ndarray
             Returns a cost matrix of shape len(targets), len(features), where
             element (i, j) contains the closest squared distance between
             `targets[i]` and `features[j]`.
-
         """
         cost_matrix = np.zeros((len(targets), len(features)))
         for i, target in enumerate(targets):
