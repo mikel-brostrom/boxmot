30e550893413a11be2c7e79b894b30932e599f3a
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-12-03 20:01:03 +0100

    set lambda to zeroä

--- deep_sort_pytorch/deep_sort/sort/tracker.py
+++ deep_sort_pytorch/deep_sort/sort/tracker.py
@@ -35,7 +35,7 @@ class Tracker:
     """
     GATING_THRESHOLD = np.sqrt(kalman_filter.chi2inv95[4])
 
-    def __init__(self, metric, max_iou_distance=0.7, max_age=20, n_init=3, _lambda=0):
+    def __init__(self, metric, max_iou_distance=0.9, max_age=30, n_init=3, _lambda=0):
         self.metric = metric
         self.max_iou_distance = max_iou_distance
         self.max_age = max_age
@@ -91,44 +91,72 @@ class Tracker:
             features += track.features
             targets += [track.track_id for _ in track.features]
             track.features = []
-        self.metric.partial_fit(
-            np.asarray(features), np.asarray(targets), active_targets)
+        self.metric.partial_fit(np.asarray(features), np.asarray(targets), active_targets)
+
+    def _full_cost_metric(self, tracks, dets, track_indices, detection_indices):
+        """
+        This implements the full lambda-based cost-metric. However, in doing so, it disregards
+        the possibility to gate the position only which is provided by
+        linear_assignment.gate_cost_matrix(). Instead, I gate by everything.
+        Note that the Mahalanobis distance is itself an unnormalised metric. Given the cosine
+        distance being normalised, we employ a quick and dirty normalisation based on the
+        threshold: that is, we divide the positional-cost by the gating threshold, thus ensuring
+        that the valid values range 0-1.
+        Note also that the authors work with the squared distance. I also sqrt this, so that it
+        is more intuitive in terms of values.
+        """
+        # Compute First the Position-based Cost Matrix
+        pos_cost = np.empty([len(track_indices), len(detection_indices)])
+        msrs = np.asarray([dets[i].to_xyah() for i in detection_indices])
+        for row, track_idx in enumerate(track_indices):
+            pos_cost[row, :] = np.sqrt(
+                self.kf.gating_distance(
+                    tracks[track_idx].mean, tracks[track_idx].covariance, msrs, False
+                )
+            ) / self.GATING_THRESHOLD
+        pos_gate = pos_cost > 1.0
+        # Now Compute the Appearance-based Cost Matrix
+        app_cost = self.metric.distance(
+            np.array([dets[i].feature for i in detection_indices]),
+            np.array([tracks[i].track_id for i in track_indices]),
+        )
+        app_gate = app_cost > self.metric.matching_threshold
+        # Now combine and threshold
+        cost_matrix = self._lambda * pos_cost + (1 - self._lambda) * app_cost
+        cost_matrix[np.logical_or(pos_gate, app_gate)] = linear_assignment.INFTY_COST
+        # Return Matrix
+        return cost_matrix
 
     def _match(self, detections):
-
-        def gated_metric(tracks, dets, track_indices, detection_indices):
-            features = np.array([dets[i].feature for i in detection_indices])
-            targets = np.array([tracks[i].track_id for i in track_indices])
-            cost_matrix = self.metric.distance(features, targets)
-            cost_matrix = linear_assignment.gate_cost_matrix(
-                self.kf, cost_matrix, tracks, dets, track_indices,
-                detection_indices)
-
-            return cost_matrix
-
         # Split track set into confirmed and unconfirmed tracks.
-        confirmed_tracks = [
-            i for i, t in enumerate(self.tracks) if t.is_confirmed()]
-        unconfirmed_tracks = [
-            i for i, t in enumerate(self.tracks) if not t.is_confirmed()]
+        confirmed_tracks = [i for i, t in enumerate(self.tracks) if t.is_confirmed()]
+        unconfirmed_tracks = [i for i, t in enumerate(self.tracks) if not t.is_confirmed()]
 
         # Associate confirmed tracks using appearance features.
-        matches_a, unmatched_tracks_a, unmatched_detections = \
-            linear_assignment.matching_cascade(
-                gated_metric, self.metric.matching_threshold, self.max_age,
-                self.tracks, detections, confirmed_tracks)
+        matches_a, unmatched_tracks_a, unmatched_detections = linear_assignment.matching_cascade(
+            self._full_cost_metric,
+            linear_assignment.INFTY_COST - 1,  # no need for self.metric.matching_threshold here,
+            self.max_age,
+            self.tracks,
+            detections,
+            confirmed_tracks,
+        )
 
         # Associate remaining tracks together with unconfirmed tracks using IOU.
         iou_track_candidates = unconfirmed_tracks + [
-            k for k in unmatched_tracks_a if
-            self.tracks[k].time_since_update == 1]
+            k for k in unmatched_tracks_a if self.tracks[k].time_since_update == 1
+        ]
         unmatched_tracks_a = [
-            k for k in unmatched_tracks_a if
-            self.tracks[k].time_since_update != 1]
-        matches_b, unmatched_tracks_b, unmatched_detections = \
-            linear_assignment.min_cost_matching(
-                iou_matching.iou_cost, self.max_iou_distance, self.tracks,
-                detections, iou_track_candidates, unmatched_detections)
+            k for k in unmatched_tracks_a if self.tracks[k].time_since_update != 1
+        ]
+        matches_b, unmatched_tracks_b, unmatched_detections = linear_assignment.min_cost_matching(
+            iou_matching.iou_cost,
+            self.max_iou_distance,
+            self.tracks,
+            detections,
+            iou_track_candidates,
+            unmatched_detections,
+        )
 
         matches = matches_a + matches_b
         unmatched_tracks = list(set(unmatched_tracks_a + unmatched_tracks_b))
