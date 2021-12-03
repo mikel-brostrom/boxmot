0588fb41913f2642ce78ee55d9a5e7f3b9df677b
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-12-03 19:26:12 +0100

    show kf predictions, not yolo

--- deep_sort_pytorch/deep_sort/deep_sort.py
+++ deep_sort_pytorch/deep_sort/deep_sort.py
@@ -22,7 +22,7 @@ class DeepSort(object):
         self.tracker = Tracker(
             metric, max_iou_distance=max_iou_distance, max_age=max_age, n_init=n_init)
 
-    def update(self, bbox_xywh, confidences, classes, ori_img, use_yolo_preds=True):
+    def update(self, bbox_xywh, confidences, classes, ori_img, use_yolo_preds=False):
         self.height, self.width = ori_img.shape[:2]
         # generate detections
         features = self._get_features(bbox_xywh, ori_img)
