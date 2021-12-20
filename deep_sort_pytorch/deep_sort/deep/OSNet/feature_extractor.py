a0c89a0cc9df013fd4f16ed0a57f55cfc14e4f7e
mikel.brostrom <mikel.brostrom@cevt.se>
2021-12-20 19:45:01 +0100

    make OSNet available as appearance descriptor

--- deep_sort_pytorch/deep_sort/deep/OSNet/feature_extractor.py
+++ deep_sort_pytorch/deep_sort/deep/OSNet/feature_extractor.py
@@ -5,19 +5,32 @@ import cv2
 import logging
 
 from .model import Net
+from .encoder import OsNetEncoder
 
 
 class Extractor(object):
     def __init__(self, model_path, use_cuda=True):
         self.net = Net(reid=True)
         self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
-        state_dict = torch.load(model_path, map_location=torch.device(self.device))[
-            'net_dict']
-        self.net.load_state_dict(state_dict)
+        self.input_width = 64
+        self.input_height = 128
+
+        self.net = OsNetEncoder(
+            input_width=self.input_width,
+            input_height=self.input_height,
+            weight_filepath="/home/mikel.brostrom/Yolov5_DeepSort_Pytorch/osnet_ibn_x1_0_msmt17_combineall_256x128_amsgrad_ep150_stp60_lr0.0015_b64_fb10_softmax_labelsmooth_flip_jitter.pth",
+            batch_size=32,
+            num_classes=2022,
+            patch_height=256,
+            patch_width=128,
+            norm_mean=[0.485, 0.456, 0.406],
+            norm_std=[0.229, 0.224, 0.225],
+            GPU=True
+        )
+
         logger = logging.getLogger("root.tracker")
         logger.info("Loading weights from {}... Done!".format(model_path))
-        self.net.to(self.device)
-        self.size = (64, 128)
+        self.size = (self.input_width, self.input_height)
         self.norm = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
@@ -33,7 +46,7 @@ class Extractor(object):
             4. normalize
         """
         def _resize(im, size):
-            return cv2.resize(im.astype(np.float32)/255., size)
+            return cv2.resize(im.astype(np.float32) / 255., size)
 
         im_batch = torch.cat([self.norm(_resize(im, self.size)).unsqueeze(
             0) for im in im_crops], dim=0).float()
@@ -43,7 +56,7 @@ class Extractor(object):
         im_batch = self._preprocess(im_crops)
         with torch.no_grad():
             im_batch = im_batch.to(self.device)
-            features = self.net(im_batch)
+            features = self.net.get_features(im_batch)
         return features.cpu().numpy()
 
 
---
+++ deep_sort_pytorch/deep_sort/deep/OSNet/image_handler.py
@@ -0,0 +1,77 @@
+from typing import List, Tuple
+import torch
+import numpy as np
+from PIL import Image
+from torch.autograd import Variable
+
+
+def resize(img: np.ndarray, size: Tuple[int, int], interpolation=Image.BILINEAR):
+    """Resize the input PIL Image to the given size.
+
+    Args:
+        img ( np.ndarray Image): Image to be resized.
+        size (tuple of int): Desired output size. The size is a sequence with the structure
+            (h, w). The output size will be matched to this. `
+        interpolation (int, optional): Desired interpolation. Default is
+            ``PIL.Image.BILINEAR``
+    Returns:
+        PIL Image: Resized image.
+    """
+    # Resizing needs "PIL.Image" objects, as scipy method was deprecated
+    # and numpy does not support these sort of image transformations
+    pil_img = Image.fromarray(img)
+    pil_img = pil_img.resize(size[::-1], interpolation)
+    return np.array(pil_img)
+
+def ndarray_to_tensor(pic: np.ndarray):
+    """Translates the ndarray to a torch Tensor
+    :param pic: PIL Image: Resized image
+    :return: transformed torch.Tensor, required by the "normalize()" method
+    """
+
+    # Define constants
+    RGB_VALUES = 255
+
+    # handle numpy array
+    if pic.ndim == 2:
+        pic = pic[:, :, None]
+    tensor = torch.from_numpy(pic.transpose((2, 0, 1)) / RGB_VALUES)
+    return tensor
+
+
+def normalize(tensor: torch.Tensor, mean: List[float], std: List[float], inplace=False):
+    """Normalize a tensor image with mean and standard deviation.
+
+    .. note::
+        This transform acts out of place by default, i.e., it does not mutates the input tensor.
+
+    See :class:`~torchvision.transforms.Normalize` for more details.
+
+    Args:
+        tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+        inplace(bool,optional): Bool to make this operation inplace.
+
+    Returns:
+        Tensor: Normalized Tensor image.
+    """
+
+    if not inplace:
+        tensor = tensor.clone()
+    # Issues leaving a tensor as torch.uint8, which is the exit of the method ndarray_to_tensor(),
+    # as tensor.sub_(mean[:, None, None]).div_(std[:, None, None]) presented an issue of division by 0.
+    # Output of torchvision.transforms.compose() is a torch.float32
+    tensor = tensor.type(torch.float32)
+    dtype = tensor.dtype
+    # Transform mean and std in torch tensors
+    mean = torch.as_tensor(mean, dtype=dtype, device=tensor.device)
+    std = torch.as_tensor(std, dtype=dtype, device=tensor.device)
+    # Normalization is implemented
+    tensor.sub_(mean[:, None, None]).div_(std[:, None, None])
+    # unsqueeze_() adds another dimension to the tensor, with shape  (:,:,:).
+    # The input of our net requires a (1,:,:,:) tensor.
+    tensor.unsqueeze_(0)
+    # "autograd.Variable()" creates tensors that support gradient calculations.
+    # Might be redundant as no backpropagation is computed in "test" mode.
+    return Variable(tensor)
