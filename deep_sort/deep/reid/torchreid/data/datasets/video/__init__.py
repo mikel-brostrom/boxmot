0d27dd60e9058ff65fb8830418a99e07060274b8
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-12-21 23:36:55 +0100

    hardcoded reid package deleted

--- deep_sort/deep/reid/torchreid/data/datasets/video/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function, absolute_import
-
-from .mars import Mars
-from .ilidsvid import iLIDSVID
-from .prid2011 import PRID2011
-from .dukemtmcvidreid import DukeMTMCVidReID
