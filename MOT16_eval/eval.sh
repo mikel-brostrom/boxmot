eb703ccd933c3868d00265efaf47fb0c68d71500
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2021-12-26 19:59:12 +0100

    updated eval based on new save

--- MOT16_eval/eval.sh
+++ MOT16_eval/eval.sh
@@ -69,7 +69,7 @@ mkdir -p ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/trackers/mot_challe
 
 # inference on 4 MOT16 video sequences at the same time
 # suits a 4GB GRAM GPU, feel free to increase if you have more memory
-N=3
+N=1
 
 # generate tracking results for each sequence
 for i in MOT16-02 MOT16-04 MOT16-05 MOT16-09 MOT16-10 MOT16-11 MOT16-13 
@@ -81,10 +81,8 @@ do
 			mv ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/img1/ ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/$i
 		fi
 		# run inference on sequence frames
-		python3 track.py --source ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/$i --save-txt --evaluate --yolo_model yolov5/weights/crowdhuman_yolov5m.pt --classes 0
+		python3 track.py --source ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/$i --save-txt --evaluate --yolo_model yolov5/weights/crowdhuman_yolov5m.pt --classes 0 --exist-ok
 	    # move generated results to evaluation repo
-	    mv ~/Yolov5_DeepSort_Pytorch/inference/output/$i.txt \
-	    ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/trackers/mot_challenge/MOT16-train/ch_yolov5m_deep_sort/data/$i.txt
 	) &
 	# https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
 	# allow to execute up to $N jobs in parallel
@@ -100,6 +98,10 @@ done
 # (all need to be finished)
 wait
 echo "Inference on all MOT16 sequences DONE"
+
+echo "Moving data from experiment folder to MOT16"
+mv ~/Yolov5_DeepSort_Pytorch/runs/track/exp/* \
+   ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/trackers/mot_challenge/MOT16-train/ch_yolov5m_deep_sort/data/
 
 # run the evaluation
 python ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/scripts/run_mot_challenge.py --BENCHMARK MOT16 \
