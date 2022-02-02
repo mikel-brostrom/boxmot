a086ea898d565495e88049c8599e69aae089d9e9
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2022-02-02 08:17:57 +0100

    cuda features to cpu

--- deep_sort/sort/detection.py
+++ deep_sort/sort/detection.py
@@ -29,7 +29,7 @@ class Detection(object):
     def __init__(self, tlwh, confidence, feature):
         self.tlwh = np.asarray(tlwh, dtype=np.float)
         self.confidence = float(confidence)
-        self.feature = np.asarray(feature, dtype=np.float32)
+        self.feature = np.asarray(feature.cpu(), dtype=np.float32)
 
     def to_tlbr(self):
         """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
