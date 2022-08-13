c7fba39548206f620a4ba352c5ccf7c734665fd6
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2022-08-13 21:51:40 +0200

    added TensorRT and TorchScript support for ReID models

--- strong_sort/reid_multibackend.py
+++ strong_sort/reid_multibackend.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import numpy as np
 import torchvision.transforms as transforms
 import cv2
+from collections import OrderedDict, namedtuple
 import gdown
 from os.path import exists as file_exists
 from .deep.reid_model_factory import show_downloadeable_models, get_model_url, get_model_name
 
+from yolov5.utils.general import LOGGER, check_version
 from torchreid.utils import FeatureExtractor
 from torchreid.utils.tools import download_url
 
@@ -53,15 +55,48 @@ class ReIDDetectMultiBackend(nn.Module):
             )
             
             self.extractor.model.half() if fp16 else  self.extractor.model.float()
+        elif self.jit:
+            LOGGER.info(f'Loading {w} for TorchScript inference...')
+            self.model = torch.jit.load(weights)
+            self.model.half() if fp16 else  self.model.float()
+            
         elif self.onnx:  # ONNX Runtime
-            # LOGGER.info(f'Loading {w} for ONNX Runtime inference...')
+            LOGGER.info(f'Loading {w} for ONNX Runtime inference...')
             cuda = torch.cuda.is_available()
             #check_requirements(('onnx', 'onnxruntime-gpu' if cuda else 'onnxruntime'))
             import onnxruntime
             providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
             self.session = onnxruntime.InferenceSession(w, providers=providers)
+            
+        elif self.engine:  # TensorRT
+            LOGGER.info(f'Loading {w} for TensorRT inference...')
+            import tensorrt as trt  # https://developer.nvidia.com/nvidia-tensorrt-download
+            check_version(trt.__version__, '7.0.0', hard=True)  # require tensorrt>=7.0.0
+            Binding = namedtuple('Binding', ('name', 'dtype', 'shape', 'data', 'ptr'))
+            logger = trt.Logger(trt.Logger.INFO)
+            with open(w, 'rb') as f, trt.Runtime(logger) as runtime:
+                model = runtime.deserialize_cuda_engine(f.read())
+            self.context = model.create_execution_context()
+            self.bindings = OrderedDict()
+            fp16 = False  # default updated below
+            self.dynamic = False
+            for index in range(model.num_bindings):
+                name = model.get_binding_name(index)
+                dtype = trt.nptype(model.get_binding_dtype(index))
+                if model.binding_is_input(index):
+                    if -1 in tuple(model.get_binding_shape(index)):  # dynamic
+                        self.dynamic = True
+                        context.set_binding_shape(index, tuple(model.get_profile_shape(0, index)[2]))
+                    if dtype == np.float16:
+                        fp16 = True
+                shape = tuple(self.context.get_binding_shape(index))
+                data = torch.from_numpy(np.empty(shape, dtype=np.dtype(dtype))).to(device)
+                self.bindings[name] = Binding(name, dtype, shape, data, int(data.data_ptr()))
+            self.binding_addrs = OrderedDict((n, d.ptr) for n, d in self.bindings.items())
+            batch_size = self.bindings['images'].shape[0]  # if dynamic, this is instead max batch size
         
         elif self.tflite:
+            LOGGER.info(f'Loading {w} for TensorFlow Lite inference...')
             try:  # https://coral.ai/docs/edgetpu/tflite-python/#update-existing-tf-lite-code-for-the-edge-tpu
                 from tflite_runtime.interpreter import Interpreter, load_delegate
             except ImportError:
@@ -81,7 +116,6 @@ class ReIDDetectMultiBackend(nn.Module):
 
             # The function `get_tensor()` returns a copy of the tensor data.
             output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-            print(output_data.shape)
         else:
             print('This model framework is not supported yet!')
             exit()
@@ -114,7 +148,6 @@ class ReIDDetectMultiBackend(nn.Module):
         if any(warmup_types) and self.device.type != 'cpu':
             im = torch.zeros(*imgsz, dtype=torch.half if self.fp16 else torch.float, device=self.device)  # input
             im = im.cpu().numpy()
-            print(im.shape)
             for _ in range(2 if self.jit else 1):  #
                 self.forward(im)  # warmup
 
@@ -139,11 +172,23 @@ class ReIDDetectMultiBackend(nn.Module):
             elif self.jit:  # TorchScript
                 y = self.model(im)[0]
             elif self.onnx:  # ONNX Runtime
-                im = im.permute(0, 1, 3, 2).cpu().numpy()  # torch to numpy  # torch to numpy
+                im = im.permute(0, 1, 3, 2).cpu().numpy()  # torch to numpy
                 y = self.session.run([self.session.get_outputs()[0].name], {self.session.get_inputs()[0].name: im})[0]
             elif self.xml:  # OpenVINO
                 im = im.cpu().numpy()  # FP32
                 y = self.executable_network([im])[self.output_layer]
+            elif self.engine:  # TensorRT
+                im = im.permute(0, 1, 3, 2)
+                if self.dynamic and im.shape != self.bindings['images'].shape:
+                    i_in, i_out = (self.model.get_binding_index(x) for x in ('images', 'output'))
+                    self.context.set_binding_shape(i_in, im.shape)  # reshape if dynamic
+                    self.bindings['images'] = self.bindings['images']._replace(shape=im.shape)
+                    self.bindings['output'].data.resize_(tuple(self.context.get_binding_shape(i_out)))
+                s = self.bindings['images'].shape
+                assert im.shape == s, f"input size {im.shape} {'>' if self.dynamic else 'not equal to'} max model size {s}"
+                self.binding_addrs['images'] = int(im.data_ptr())
+                self.context.execute_v2(list(self.binding_addrs.values()))
+                y = self.bindings['output'].data
             else:  # TensorFlow (SavedModel, GraphDef, Lite, Edge TPU)
                 im = im.permute(0, 3, 2, 1).cpu().numpy()  # torch BCHW to numpy BHWC shape(1,320,192,3)
                 input, output = self.input_details[0], self.output_details[0]
