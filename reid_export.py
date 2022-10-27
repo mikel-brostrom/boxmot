e5b004fb32be238aaa34d214dcfe994303a8bfcb
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2022-10-27 10:57:55 +0200

    deelted old engine export function

--- reid_export.py
+++ reid_export.py
@@ -64,64 +64,6 @@ def export_formats():
     return pd.DataFrame(x, columns=['Format', 'Argument', 'Suffix', 'CPU', 'GPU'])
 
 
-def export_engine(model, im, file, half, dynamic, simplify, workspace=4, verbose=False):
-    assert im.device.type != 'cpu', 'export running on CPU but must be on GPU, i.e. `python export.py --device 0`'
-    try:
-        import tensorrt as trt
-    except Exception:
-        if platform.system() == 'Linux':
-            check_requirements(('nvidia-tensorrt',), cmds=('-U --index-url https://pypi.ngc.nvidia.com',))
-        import tensorrt as trt
-
-    if trt.__version__[0] == '7':  # TensorRT 7 handling https://github.com/ultralytics/yolov5/issues/6012
-        export_onnx(model, im, file, 12, False, dynamic, simplify)  # opset 12
-    else:  # TensorRT >= 8
-        check_version(trt.__version__, '8.0.0', hard=True)  # require tensorrt>=8.0.0
-        export_onnx(model, im, file, 13, False, dynamic, simplify)  # opset 13
-    onnx = file.with_suffix('.onnx')
-
-    LOGGER.info(f'\n{prefix} starting export with TensorRT {trt.__version__}...')
-    assert onnx.exists(), f'failed to export ONNX file: {onnx}'
-    f = file.with_suffix('.engine')  # TensorRT engine file
-    logger = trt.Logger(trt.Logger.INFO)
-    if verbose:
-        logger.min_severity = trt.Logger.Severity.VERBOSE
-
-    builder = trt.Builder(logger)
-    config = builder.create_builder_config()
-    config.max_workspace_size = workspace * 1 << 30
-    # config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, workspace << 30)  # fix TRT 8.4 deprecation notice
-
-    flag = (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
-    network = builder.create_network(flag)
-    parser = trt.OnnxParser(network, logger)
-    if not parser.parse_from_file(str(onnx)):
-        raise RuntimeError(f'failed to load ONNX file: {onnx}')
-
-    inputs = [network.get_input(i) for i in range(network.num_inputs)]
-    outputs = [network.get_output(i) for i in range(network.num_outputs)]
-    LOGGER.info(f'{prefix} Network Description:')
-    for inp in inputs:
-        LOGGER.info(f'{prefix}\tinput "{inp.name}" with shape {inp.shape} and dtype {inp.dtype}')
-    for out in outputs:
-        LOGGER.info(f'{prefix}\toutput "{out.name}" with shape {out.shape} and dtype {out.dtype}')
-
-    if dynamic:
-        if im.shape[0] <= 1:
-            LOGGER.warning(f"{prefix}WARNING: --dynamic model requires maximum --batch-size argument")
-        profile = builder.create_optimization_profile()
-        for inp in inputs:
-            profile.set_shape(inp.name, (1, *im.shape[1:]), (max(1, im.shape[0] // 2), *im.shape[1:]), im.shape)
-        config.add_optimization_profile(profile)
-
-    LOGGER.info(f'{prefix} building FP{16 if builder.platform_has_fast_fp16 and half else 32} engine in {f}')
-    if builder.platform_has_fast_fp16 and half:
-        config.set_flag(trt.BuilderFlag.FP16)
-    with builder.build_engine(network, config) as engine, open(f, 'wb') as t:
-        t.write(engine.serialize())
-    return f, None
-
-
 def export_torchscript(model, im, file, optimize, prefix=colorstr('TorchScript:')):
     # YOLOv5 TorchScript model export
     try:
@@ -331,7 +273,7 @@ if __name__ == "__main__":
         num_classes=1,
         pretrained=not (args.weights and args.weights.is_file() and args.weights.suffix == '.pt'),
         use_gpu=args.device
-    )
+    ).to(args.device)
     load_pretrained_weights(model, args.weights)
 
     include = [x.lower() for x in args.include]  # to lowercase
@@ -341,6 +283,8 @@ if __name__ == "__main__":
     jit, onnx, openvino, engine, tflite = flags  # export booleans
     
     im = torch.zeros(args.batch_size, 3, args.imgsz[0], args.imgsz[1]).to(args.device)  # image size(1,3,640,480) BCHW iDetection
+    print(type(im))
+    print(type(model))
     for _ in range(2):
         y = model.eval()(im)  # dry runs
     if args.half:
