577635f60d864a15e434e8c5b4c134992049e508
mikel.brostrom <mikel.brostrom@cevt.se>
2022-11-08 18:39:07 +0000

    update model

--- val.py
+++ val.py
@@ -60,7 +60,7 @@ def setup_evaluation(dst_val_tools_folder, benchmark):
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--yolo-weights', nargs='+', type=str, default=WEIGHTS / 'crowdhuman_yolov5m.pt', help='model.pt path(s)')
-    parser.add_argument('--reid-weights', type=str, default=WEIGHTS / 'mobilenetv2_x1_0_msmt17.pt')
+    parser.add_argument('--reid-weights', type=str, default=WEIGHTS / 'osnet_x1_0_dukemtmcreid.pt')
     parser.add_argument('--tracking-method', type=str, default='strongsort', help='strongsort, ocsort')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--project', default=ROOT / 'runs/track', help='save results to project/name')
