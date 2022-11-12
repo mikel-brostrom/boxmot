92cd8447b915e5aea87b398b787d60f1f0be95e7
mikel.brostrom <mikel.brostrom@cevt.se>
2022-11-12 20:15:53 +0100

    delete not needed sys.path appends

--- track.py
+++ track.py
@@ -24,10 +24,7 @@ if str(ROOT / 'yolov5') not in sys.path:
     sys.path.append(str(ROOT / 'yolov5'))  # add yolov5 ROOT to PATH
 if str(ROOT / 'trackers' / 'strong_sort') not in sys.path:
     sys.path.append(str(ROOT / 'trackers' / 'strong_sort'))  # add strong_sort ROOT to PATH
-if str(ROOT / 'trackers' / 'ocsort') not in sys.path:
-    sys.path.append(str(ROOT / 'trackers' / 'ocsort'))  # add strong_sort ROOT to PATH
-if str(ROOT / 'trackers' / 'strong_sort' / 'deep' / 'reid' / 'torchreid') not in sys.path:
-    sys.path.append(str(ROOT / 'trackers' / 'strong_sort' / 'deep' / 'reid' / 'torchreid'))  # add strong_sort ROOT to PATH
+
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 import logging
