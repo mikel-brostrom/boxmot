dc5c6039f648ae8af08bb1c2a899217f76c6a5ca
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2022-12-19 07:15:53 +0000

    float now float32

--- trackers/strong_sort/sort/detection.py
+++ trackers/strong_sort/sort/detection.py
@@ -27,7 +27,7 @@ class Detection(object):
     """
 
     def __init__(self, tlwh, confidence, feature):
-        self.tlwh = np.asarray(tlwh, dtype=np.float)
+        self.tlwh = np.asarray(tlwh, dtype=np.float32)
         self.confidence = float(confidence)
         self.feature = np.asarray(feature.cpu(), dtype=np.float32)
 
