81356d89ae210626b6f14392d7b99d39a5026919
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2022-12-21 16:13:25 +0000

    np.float now np.float32

--- trackers/bytetrack/matching.py
+++ trackers/bytetrack/matching.py
@@ -117,13 +117,13 @@ def embedding_distance(tracks, detections, metric='cosine'):
     :return: cost_matrix np.ndarray
     """
 
-    cost_matrix = np.zeros((len(tracks), len(detections)), dtype=np.float)
+    cost_matrix = np.zeros((len(tracks), len(detections)), dtype=np.float32)
     if cost_matrix.size == 0:
         return cost_matrix
-    det_features = np.asarray([track.curr_feat for track in detections], dtype=np.float)
+    det_features = np.asarray([track.curr_feat for track in detections], dtype=np.float32)
     #for i, track in enumerate(tracks):
         #cost_matrix[i, :] = np.maximum(0.0, cdist(track.smooth_feat.reshape(1,-1), det_features, metric))
-    track_features = np.asarray([track.smooth_feat for track in tracks], dtype=np.float)
+    track_features = np.asarray([track.smooth_feat for track in tracks], dtype=np.float32)
     cost_matrix = np.maximum(0.0, cdist(track_features, det_features, metric))  # Nomalized features
     return cost_matrix
 
@@ -192,7 +192,7 @@ def bbox_ious(boxes, query_boxes):
     """
     N = boxes.shape[0]
     K = query_boxes.shape[0]
-    overlaps = np.zeros((N, K), dtype=DTYPE)
+    overlaps = np.zeros((N, K), dtype=np.float32)
     
     for k in range(K):
         box_area = (
