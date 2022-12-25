ff3a0eaa71e3f6d4df66a94763cc7c901e2c5b0f
mikel.brostrom <mikel.brostrom@cevt.se>
2022-12-25 16:13:50 +0000

    get rid of pyx

--- trackers/botsort/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-package = Extension('c_bbox', ['c_bbox.pyx'], include_dirs=[numpy.get_include()])
-setup(ext_modules=cythonize([package]))
