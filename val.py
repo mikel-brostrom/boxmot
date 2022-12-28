54fb1fa6f766a89b48986a4df171c9b084176675
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2022-12-28 22:23:29 +0000

    optuna toy example working: WIP

--- val.py
+++ val.py
@@ -7,6 +7,7 @@ from subprocess import Popen
 import argparse
 from io import StringIO
 import git
+import optuna
 import re
 import pandas as pd
 from git import Repo
@@ -125,124 +126,138 @@ def parse_opt():
     return opt
 
 
-def main(opt):
-    check_requirements(requirements=ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
+class Objective:
+    def __init__(self, opts):  
+        self.opt = opts  
     
-    # download eval files
-    val_tools_target_location = ROOT / 'val_utils'
-    download_official_mot_eval_tool(val_tools_target_location)
-    
-    if any(opt.benchmark == s for s in ['MOT16', 'MOT17', 'MOT20']):
-        download_mot_dataset(val_tools_target_location, opt.benchmark)
-    
-    # set paths
-    mot_seqs_path = val_tools_target_location / 'data' / opt.benchmark / opt.split
-    
-    if opt.benchmark == 'MOT17':
-        # each sequences is present 3 times, one for each detector
-        # (DPM, FRCNN, SDP). Keep only sequences from  one of them
-        seq_paths = sorted([str(p / 'img1') for p in Path(mot_seqs_path).iterdir() if Path(p).is_dir()])
-        seq_paths = [Path(p) for p in seq_paths if 'FRCNN' in p]
-        with open(val_tools_target_location / "data/gt/mot_challenge/seqmaps/MOT17-train.txt", "r") as f:  # 
-            lines = f.readlines()
-        # overwrite MOT17 evaluation sequences to evaluate so that they are not duplicated
-        with open(val_tools_target_location / "data/gt/mot_challenge/seqmaps/MOT17-train.txt", "w") as f:
-            for line in seq_paths:
-                f.write(str(line.parent.stem) + '\n')
-    else:
-        # this is not the case for MOT16, MOT20 or your custom dataset
-        seq_paths = [p / 'img1' for p in Path(mot_seqs_path).iterdir() if Path(p).is_dir()]
-    
-    save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok)  # increment run
-    MOT_results_folder = val_tools_target_location / 'data' / 'trackers' / 'mot_challenge' / Path(str(opt.benchmark) + '-' + str(opt.split)) / save_dir.name / 'data'
-    (MOT_results_folder).mkdir(parents=True, exist_ok=True)  # make
+    def __call__(self, trial):
+        # Calculate an objective value by using the extra arguments.
+        self.opt.conf_thres = trial.suggest_float("conf_thres", 0.3, 0.6)
+        
+        # download eval files
+        val_tools_target_location = ROOT / 'val_utils'
+        download_official_mot_eval_tool(val_tools_target_location)
+        
+        if any(self.opt.benchmark == s for s in ['MOT16', 'MOT17', 'MOT20']):
+            download_mot_dataset(val_tools_target_location, self.opt.benchmark)
+        
+        # set paths
+        mot_seqs_path = val_tools_target_location / 'data' / self.opt.benchmark / self.opt.split
+        
+        if self.opt.benchmark == 'MOT17':
+            # each sequences is present 3 times, one for each detector
+            # (DPM, FRCNN, SDP). Keep only sequences from  one of them
+            seq_paths = sorted([str(p / 'img1') for p in Path(mot_seqs_path).iterdir() if Path(p).is_dir()])
+            seq_paths = [Path(p) for p in seq_paths if 'FRCNN' in p]
+            with open(val_tools_target_location / "data/gt/mot_challenge/seqmaps/MOT17-train.txt", "r") as f:  # 
+                lines = f.readlines()
+            # overwrite MOT17 evaluation sequences to evaluate so that they are not duplicated
+            with open(val_tools_target_location / "data/gt/mot_challenge/seqmaps/MOT17-train.txt", "w") as f:
+                for line in seq_paths:
+                    f.write(str(line.parent.stem) + '\n')
+        else:
+            # this is not the case for MOT16, MOT20 or your custom dataset
+            seq_paths = [p / 'img1' for p in Path(mot_seqs_path).iterdir() if Path(p).is_dir()]
+        
+        save_dir = increment_path(Path(self.opt.project) / self.opt.name, exist_ok=self.opt.exist_ok)  # increment run
+        MOT_results_folder = val_tools_target_location / 'data' / 'trackers' / 'mot_challenge' / Path(str(self.opt.benchmark) + '-' + str(self.opt.split)) / save_dir.name / 'data'
+        (MOT_results_folder).mkdir(parents=True, exist_ok=True)  # make
 
-    # extend devices to as many sequences are available
-    if any(isinstance(i,int) for i in opt.device) and len(opt.device) > 1:
-        devices = opt.device
-        for a in range(0, len(opt.device) % len(seq_paths)):
-            opt.device.extend(devices)
-        opt.device = opt.device[:len(seq_paths)]
- 
-    if not opt.eval_existing:
-        processes = []
-        free_devices = opt.device * opt.processes_per_device
-        busy_devices = []
-        for i, seq_path in enumerate(seq_paths):
-            # spawn one subprocess per GPU in increasing order.
-            # When max devices are reached start at 0 again
-            if i > 0 and len(free_devices) == 0:
-                if len(processes) == 0:
-                    raise IndexError("No active processes and no devices available.")
+        # extend devices to as many sequences are available
+        if any(isinstance(i,int) for i in self.opt.device) and len(self.opt.device) > 1:
+            devices = self.opt.device
+            for a in range(0, len(self.opt.device) % len(seq_paths)):
+                self.opt.device.extend(devices)
+            self.opt.device = self.opt.device[:len(seq_paths)]
+    
+        if not self.opt.eval_existing:
+            processes = []
+            free_devices = self.opt.device * self.opt.processes_per_device
+            busy_devices = []
+            for i, seq_path in enumerate(seq_paths):
+                # spawn one subprocess per GPU in increasing order.
+                # When max devices are reached start at 0 again
+                if i > 0 and len(free_devices) == 0:
+                    if len(processes) == 0:
+                        raise IndexError("No active processes and no devices available.")
+                    
+                    # Wait for oldest process to finish so we can get a free device
+                    processes.pop(0).wait()
+                    free_devices.append(busy_devices.pop(0))
                 
-                # Wait for oldest process to finish so we can get a free device
-                processes.pop(0).wait()
-                free_devices.append(busy_devices.pop(0))
+                tracking_subprocess_device = free_devices.pop(0)
+                busy_devices.append(tracking_subprocess_device)
             
-            tracking_subprocess_device = free_devices.pop(0)
-            busy_devices.append(tracking_subprocess_device)
-        
-            dst_seq_path = seq_path.parent / seq_path.parent.name
-            if not dst_seq_path.is_dir():
-                src_seq_path = seq_path
-                shutil.move(str(src_seq_path), str(dst_seq_path))   
+                dst_seq_path = seq_path.parent / seq_path.parent.name
+                if not dst_seq_path.is_dir():
+                    src_seq_path = seq_path
+                    shutil.move(str(src_seq_path), str(dst_seq_path))   
+                
+                p = subprocess.Popen([
+                    sys.executable, "track.py",
+                    "--yolo-weights", self.opt.yolo_weights,
+                    "--reid-weights",  self.opt.reid_weights,
+                    "--tracking-method", self.opt.tracking_method,
+                    "--conf-thres", str(self.opt.conf_thres),
+                    "--imgsz", str(self.opt.imgsz[0]),
+                    "--classes", str(0),
+                    "--name", save_dir.name,
+                    "--project", self.opt.project,
+                    "--device", str(tracking_subprocess_device),
+                    "--source", dst_seq_path,
+                    "--exist-ok",
+                    "--save-txt",
+                ])
+                processes.append(p)
             
-            p = subprocess.Popen([
-                sys.executable, "track.py",
-                "--yolo-weights", opt.yolo_weights,
-                "--reid-weights",  opt.reid_weights,
-                "--tracking-method", opt.tracking_method,
-                "--conf-thres", str(opt.conf_thres),
-                "--imgsz", str(opt.imgsz[0]),
-                "--classes", str(0),
-                "--name", save_dir.name,
-                "--project", opt.project,
-                "--device", str(tracking_subprocess_device),
-                "--source", dst_seq_path,
-                "--exist-ok",
-                "--save-txt",
-            ])
-            processes.append(p)
-        
-        for p in processes:
-            p.wait()
-            
-    print_args(vars(opt))
+            for p in processes:
+                p.wait()
+                
+        print_args(vars(self.opt))
 
-    results = (save_dir.parent / opt.eval_existing / 'tracks' if opt.eval_existing else save_dir / 'tracks').glob('*.txt')
-    for src in results:
-        if opt.eval_existing:
-            dst = MOT_results_folder.parent.parent / opt.eval_existing / 'data' / Path(src.stem + '.txt')
-        else:  
-            dst = MOT_results_folder / Path(src.stem + '.txt')
-        dst.parent.mkdir(parents=True, exist_ok=True)  # make
-        shutil.copyfile(src, dst)
+        results = (save_dir.parent / self.opt.eval_existing / 'tracks' if self.opt.eval_existing else save_dir / 'tracks').glob('*.txt')
+        for src in results:
+            if self.opt.eval_existing:
+                dst = MOT_results_folder.parent.parent / self.opt.eval_existing / 'data' / Path(src.stem + '.txt')
+            else:  
+                dst = MOT_results_folder / Path(src.stem + '.txt')
+            dst.parent.mkdir(parents=True, exist_ok=True)  # make
+            shutil.copyfile(src, dst)
 
-    # run the evaluation on the generated txts
-    p = subprocess.run(
-        args=[
-            sys.executable,  val_tools_target_location / "scripts/run_mot_challenge.py",
-            "--BENCHMARK", opt.benchmark,
-            "--TRACKERS_TO_EVAL",  opt.eval_existing if opt.eval_existing else MOT_results_folder.parent.name,
-            "--SPLIT_TO_EVAL", "train",
-            "--METRICS", "HOTA", "CLEAR", "Identity",
-            "--USE_PARALLEL", "True",
-            "--NUM_PARALLEL_CORES", "4"
-        ],
-        universal_newlines=True,
-        stdout=subprocess.PIPE
-    )
-    print(p.stdout)
+        # run the evaluation on the generated txts
+        p = subprocess.run(
+            args=[
+                sys.executable,  val_tools_target_location / "scripts/run_mot_challenge.py",
+                "--BENCHMARK", self.opt.benchmark,
+                "--TRACKERS_TO_EVAL",  self.opt.eval_existing if self.opt.eval_existing else MOT_results_folder.parent.name,
+                "--SPLIT_TO_EVAL", "train",
+                "--METRICS", "HOTA", "CLEAR", "Identity",
+                "--USE_PARALLEL", "True",
+                "--NUM_PARALLEL_CORES", "4"
+            ],
+            universal_newlines=True,
+            stdout=subprocess.PIPE
+        )
+        print(p.stdout)
 
-    # get HOTA, MOTA, IDF1, Dets COMBINED results
-    combined_results = p.stdout.split('COMBINED')[2:-1]
-    # robust way of getting first ints/float in string
-    combined_results = [re.findall("[-+]?(?:\d*\.*\d+)", f)[0] for f in combined_results]
-    # pack everything in dict
-    combined_results = {key: value for key, value in zip(['HOTA', 'MOTA', 'IDF1'], combined_results)}
-
+        # get HOTA, MOTA, IDF1, Dets COMBINED results
+        combined_results = p.stdout.split('COMBINED')[2:-1]
+        # robust way of getting first ints/float in string
+        combined_results = [re.findall("[-+]?(?:\d*\.*\d+)", f)[0] for f in combined_results]
+        # pack everything in dict
+        combined_results = {key: float(value) for key, value in zip(['HOTA', 'MOTA', 'IDF1'], combined_results)}
+        return combined_results['HOTA'], combined_results['MOTA'], combined_results['IDF1']
     
 
 if __name__ == "__main__":
     opt = parse_opt()
-    main(opt)
+    #objective(opt)
+    check_requirements(requirements=ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
+    
+    objective_num = 3
+    study = optuna.create_study(directions=['maximize']*objective_num)
+    study.optimize(Objective(opt), n_trials=10)
+    fig = optuna.visualization.plot_pareto_front(study, target_names=["HOTA", "MOTA", "IDF1"])
+    fig.show()
+    fig = optuna.visualization.plot_param_importances(study, target=lambda t: t.values[0], target_name="HOTA")
+    fig.show()
