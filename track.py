cd12ff959a1bf75a1e4f30fe5e2701eb93338e3a
Riu Cherdchusakulchai <RiuC@arv.co.th>
2023-02-18 02:03:19 +0700

    refactor track.py to input yaml instead of too many argparse + modify output result

--- track.py
+++ track.py
@@ -14,17 +14,13 @@ import numpy as np
 from pathlib import Path
 import torch
 import torch.backends.cudnn as cudnn
+import yaml
 
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]  # yolov5 strongsort root directory
-WEIGHTS = ROOT / 'weights'
+ROOT = FILE.parents[0] 
 
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
-if str(ROOT / 'yolov8') not in sys.path:
-    sys.path.append(str(ROOT / 'yolov8'))  # add yolov5 ROOT to PATH
-if str(ROOT / 'trackers' / 'strongsort') not in sys.path:
-    sys.path.append(str(ROOT / 'trackers' / 'strongsort'))  # add strong_sort ROOT to PATH
 
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
@@ -35,7 +31,7 @@ from yolov8.ultralytics.yolo.data.utils import IMG_FORMATS, VID_FORMATS
 from yolov8.ultralytics.yolo.utils import DEFAULT_CFG, LOGGER, SETTINGS, callbacks, colorstr, ops
 from yolov8.ultralytics.yolo.utils.checks import check_file, check_imgsz, check_imshow, print_args, check_requirements
 from yolov8.ultralytics.yolo.utils.files import increment_path
-from yolov8.ultralytics.yolo.utils.torch_utils import select_device
+from yolov8.ultralytics.yolo.utils.torch_utils import select_device, strip_optimizer
 from yolov8.ultralytics.yolo.utils.ops import Profile, non_max_suppression, scale_boxes, process_mask, process_mask_native
 from yolov8.ultralytics.yolo.utils.plotting import Annotator, colors, save_one_box
 
@@ -45,8 +41,8 @@ from trackers.multi_tracker_zoo import create_tracker
 @torch.no_grad()
 def run(
         source='0',
-        yolo_weights=WEIGHTS / 'yolov5m.pt',  # model.pt path(s),
-        reid_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
+        yolo_weights='weights/yolov8/yolov5m.pt',  # model.pt path(s),
+        reid_weights='weights/reid/osnet_x0_25_msmt17.pt',  # model.pt path,
         tracking_method='strongsort',
         tracking_config=None,
         imgsz=(640, 640),  # inference size (height, width)
@@ -60,15 +56,12 @@ def run(
         save_crop=False,  # save cropped prediction boxes
         save_trajectories=False,  # save trajectories for each track
         save_vid=False,  # save confidences in --save-txt labels
-        nosave=False,  # do not save images/videos
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
-        visualize=False,  # visualize features
         update=False,  # update all models
-        project=ROOT / 'runs' / 'track',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
+        save_dir='runs/track/exp', # output save directory
+        save_log=True,
         line_thickness=2,  # bounding box thickness (pixels)
         hide_labels=False,  # hide labels
         hide_conf=False,  # hide confidences
@@ -76,33 +69,28 @@ def run(
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
         vid_stride=1,  # video frame-rate stride
-        retina_masks=False,
+        retina_masks=False, 
+        **kwargs
 ):
+    if save_log:
+        LOGGER.setLevel(logging.INFO)
+        fh = logging.FileHandler(os.path.join(opt.save_dir, 'logging', 'info.log'))
+        fh.setLevel(logging.INFO)
+        LOGGER.addHandler(fh)
 
     source = str(source)
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (VID_FORMATS)
     is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
     webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file)
     if is_url and is_file:
         source = check_file(source)  # download
 
-    # Directories
-    if not isinstance(yolo_weights, list):  # single yolo model
-        exp_name = yolo_weights.stem
-    elif type(yolo_weights) is list and len(yolo_weights) == 1:  # single models after --yolo_weights
-        exp_name = Path(yolo_weights[0]).stem
-    else:  # multiple models after --yolo_weights
-        exp_name = 'ensemble'
-    exp_name = name if name else exp_name + "_" + reid_weights.stem
-    save_dir = increment_path(Path(project) / exp_name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'tracks' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
     # Load model
-    device = select_device(device)
+    device = select_device(opt.device)
     is_seg = '-seg' in str(yolo_weights)
     model = AutoBackend(yolo_weights, device=device, dnn=dnn, fp16=half)
     stride, names, pt = model.stride, model.names, model.pt
+    imgsz *= 2 if len(imgsz) == 1 else 1  # expand
     imgsz = check_imgsz(imgsz, stride=stride)  # check image size
 
     # Dataloader
@@ -130,7 +118,7 @@ def run(
     vid_path, vid_writer, txt_path = [None] * bs, [None] * bs, [None] * bs
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
 
-    # Create as many strong sort instances as there are video sources
+    # Create as many tracker instances as there are video sources
     tracker_list = []
     for i in range(bs):
         tracker = create_tracker(tracking_method, tracking_config, reid_weights, device, half)
@@ -146,7 +134,7 @@ def run(
     curr_frames, prev_frames = [None] * bs, [None] * bs
     for frame_idx, batch in enumerate(dataset):
         path, im, im0s, vid_cap, s = batch
-        visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if visualize else False
+        # visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if visualize else False
         with dt[0]:
             im = torch.from_numpy(im).to(device)
             im = im.half() if half else im.float()  # uint8 to fp16/32
@@ -156,7 +144,8 @@ def run(
 
         # Inference
         with dt[1]:
-            preds = model(im, augment=augment, visualize=visualize)
+            # preds = model(im, augment=augment, visualize=visualize)
+            preds = model(im, augment=augment)
 
         # Apply NMS
         with dt[2]:
@@ -217,7 +206,7 @@ def run(
                     n = (det[:, 5] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                # pass detections to strongsort
+                # pass detections to tracker
                 with dt[3]:
                     outputs[i] = tracker_list[i].update(det.cpu(), im0)
                 
@@ -298,7 +287,6 @@ def run(
                 vid_writer[i].write(im0)
 
             prev_frames[i] = curr_frames[i]
-            
         # Print total time (preprocessing + inference + NMS + tracking)
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{sum([dt.dt for dt in dt if hasattr(dt, 'dt')]) * 1E3:.1f}ms")
 
@@ -311,54 +299,85 @@ def run(
     if update:
         strip_optimizer(yolo_weights)  # update model (to fix SourceChangeWarning)
 
+def initialize_path(opt):
+    # Directories
+    project = Path(opt.output).parent
+    name =  Path(opt.output).stem
+    if not isinstance(opt.yolo_weights, list):  # single yolo model
+        exp_name = Path(opt.yolo_weights).stem
+    elif type(opt.yolo_weights) is list and len(opt.yolo_weights) == 1:  # single models after --yolo_weights
+        exp_name = Path(opt.yolo_weights[0]).stem
+    else:  # multiple models after --yolo_weights
+        exp_name = 'ensemble'
+    exp_name = name if name else exp_name + "_" + opt.reid_weights.stem
+    save_dir = increment_path(project / exp_name, exist_ok=opt.exist_ok)  # increment run
+    (save_dir / 'tracks' if opt.save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    (save_dir / 'config' if opt.save_config else save_dir).mkdir(parents=True, exist_ok=True)
+    (save_dir / 'logging' if opt.save_log else save_dir).mkdir(parents=True, exist_ok=True)
+    if opt.save_config:
+        with open(os.path.join(save_dir, 'config', 'config.yml'), 'w') as outfile:
+            yaml.dump(vars(opt), outfile)
+
+    opt.project = project
+    opt.name = name
+    opt.save_dir = save_dir
+    
+
+def set_default_value(opt, opt_list, key, value):
+    if key.upper() not in opt_list and key not in opt_list:
+        setattr(opt, key, value)
+
+def initialize_config(opt):
+    opt_list = vars(opt).keys()
+    set_default_value(opt, opt_list, 'yolo_weights', str(ROOT / 'weights/yolov8/yolov5m.pt'))
+    set_default_value(opt, opt_list, 'reid_weights', str(ROOT / 'weights/reid/osnet_x0_25_msmt17.pt'))
+    set_default_value(opt, opt_list, 'tracking_method', 'strongsort')
+    set_default_value(opt, opt_list, 'tracking_config', str(ROOT / 'trackers' / opt.tracking_method / 'configs' / (opt.tracking_method + '.yaml')))
+    set_default_value(opt, opt_list, 'imgsz', [640,])
+    set_default_value(opt, opt_list, 'conf_thres', 0.5)
+    set_default_value(opt, opt_list, 'iou_thres', 0.5)
+    set_default_value(opt, opt_list, 'max_det', 1000)
+    set_default_value(opt, opt_list, 'device', 'cpu')
+    set_default_value(opt, opt_list, 'show_vid', False)
+    set_default_value(opt, opt_list, 'save_txt', False)
+    set_default_value(opt, opt_list, 'save_config', False)
+    set_default_value(opt, opt_list, 'save_log', False)
+    set_default_value(opt, opt_list, 'save_conf', False)
+    set_default_value(opt, opt_list, 'save_crop', False)
+    set_default_value(opt, opt_list, 'save_trajectories', False)
+    set_default_value(opt, opt_list, 'save_vid', False)
+    set_default_value(opt, opt_list, 'classes', None)
+    set_default_value(opt, opt_list, 'agnostic_nmsp', False)
+    set_default_value(opt, opt_list, 'augment', False)
+    set_default_value(opt, opt_list, 'update', False)
+    set_default_value(opt, opt_list, 'exist_ok', False)
+    set_default_value(opt, opt_list, 'line_thickness', 2)
+    set_default_value(opt, opt_list, 'hide_labels', False)
+    set_default_value(opt, opt_list, 'hide_conf', False)
+    set_default_value(opt, opt_list, 'hide_class', False)
+    set_default_value(opt, opt_list, 'half', False)
+    set_default_value(opt, opt_list, 'dnn', False)
+    set_default_value(opt, opt_list, 'vid_stride', 1)
+    set_default_value(opt, opt_list, 'retina_masks', False)
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo-weights', nargs='+', type=Path, default=WEIGHTS / 'yolov8s-seg.pt', help='model.pt path(s)')
-    parser.add_argument('--reid-weights', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt')
-    parser.add_argument('--tracking-method', type=str, default='bytetrack', help='strongsort, ocsort, bytetrack')
-    parser.add_argument('--tracking-config', type=Path, default=None)
     parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob, 0 for webcam')  
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.5, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show-vid', action='store_true', help='display tracking video results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--save-trajectories', action='store_true', help='save trajectories for each track')
-    parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs' / 'track', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=2, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--hide-class', default=False, action='store_true', help='hide IDs')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    parser.add_argument('--retina-masks', action='store_true', help='whether to plot masks in native resolution')
+    parser.add_argument('--output', type=str, default= str(ROOT / 'runs' / 'track' / 'exp'), help='output folder for the result')  
+    parser.add_argument('--track_config', type=str, default='track_configs/default.yml', help='config file name')
     opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    opt.tracking_config = ROOT / 'trackers' / opt.tracking_method / 'configs' / (opt.tracking_method + '.yaml')
-    print_args(vars(opt))
-    return opt
 
+    with open(opt.track_config, 'r') as outfile :
+        track_config = yaml.safe_load(outfile)
+    for key in track_config:
+        setattr(opt, key, track_config[key])
+    return opt
 
 def main(opt):
     check_requirements(requirements=ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
+    initialize_config(opt)
+    initialize_path(opt)
     run(**vars(opt))
-
 
 if __name__ == "__main__":
     opt = parse_opt()
