2b485b18df93143ccceb75d105e33585e549be92
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-05-10 14:32:59 +0000

    make lmbn weights loadable

--- trackers/strongsort/deep/reid_model_factory.py
+++ trackers/strongsort/deep/reid_model_factory.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 __model_types = [
     'resnet50', 'mlfn', 'hacnn', 'mobilenetv2_x1_0', 'mobilenetv2_x1_4',
     'osnet_x1_0', 'osnet_x0_75', 'osnet_x0_5', 'osnet_x0_25',
-    'osnet_ibn_x1_0', 'osnet_ain_x1_0']
+    'osnet_ibn_x1_0', 'osnet_ain_x1_0', 'lmbn_n']
 
 __trained_urls = {
 
@@ -179,37 +179,38 @@ def load_pretrained_weights(model, weight_path):
         state_dict = checkpoint
 
     model_dict = model.state_dict()
-    new_state_dict = OrderedDict()
-    matched_layers, discarded_layers = [], []
+    # new_state_dict = OrderedDict()
+    # matched_layers, discarded_layers = [], []
+    # [print(k) for k,v in state_dict.items()]
 
-    for k, v in state_dict.items():
-        if k.startswith('module.'):
-            k = k[7:] # discard module.
+    # for k, v in state_dict.items():
+    #     if k.startswith('module.'):
+    #         k = k[7:] # discard module.
 
-        if k in model_dict and model_dict[k].size() == v.size():
-            new_state_dict[k] = v
-            matched_layers.append(k)
-        else:
-            discarded_layers.append(k)
+    #     if k in model_dict and model_dict[k].size() == v.size():
+    #         new_state_dict[k] = v
+    #         matched_layers.append(k)
+    #     else:
+    #         discarded_layers.append(k)
 
-    model_dict.update(new_state_dict)
-    model.load_state_dict(model_dict)
+    # model_dict.update(new_state_dict)
+    model.load_state_dict(model_dict, strict=True)
 
-    if len(matched_layers) == 0:
-        warnings.warn(
-            'The pretrained weights "{}" cannot be loaded, '
-            'please check the key names manually '
-            '(** ignored and continue **)'.format(weight_path)
-        )
-    else:
-        print(
-            'Successfully loaded pretrained weights from "{}"'.
-            format(weight_path)
-        )
-        if len(discarded_layers) > 0:
-            print(
-                '** The following layers are discarded '
-                'due to unmatched keys or layer size: {}'.
-                format(discarded_layers)
-            )
+    # if len(matched_layers) == 0:
+    #     print(
+    #         'The pretrained weights "{}" cannot be loaded, '
+    #         'please check the key names manually '
+    #         '(** ignored and continue **)'.format(weight_path)
+    #     )
+    # else:
+    #     print(
+    #         'Successfully loaded pretrained weights from "{}"'.
+    #         format(weight_path)
+    #     )
+    #     if len(discarded_layers) > 0:
+    #         print(
+    #             '** The following layers are discarded '
+    #             'due to unmatched keys or layer size: {}'.
+    #             format(discarded_layers)
+    #         )
 
