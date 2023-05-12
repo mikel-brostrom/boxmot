b9b796de25e8c95673d74bb39c26205da60e0034
mikel.brostrom <mikel.brostrom@cevt.se>
2023-05-12 20:21:01 +0200

    WIP: track based on ultralytics

--- track_v2.py
+++ track_v2.py
@@ -25,24 +25,17 @@ STREAM_WARNING = """
             probs = r.probs  # Class probabilities for classification outputs
 """
 
-def on_predict_postprocess_end(trackers,results,frame):
-    # '''
-    bs = 1
-    im0s = frame
-    im0s = im0s if isinstance(im0s, list) else [im0s]
-    for i in range(bs):
-        det = results[i].boxes.cpu().numpy()
-        if len(det) == 0:
-            continue
-        tracks = trackers[i].update(det, im0s[i])
-        if len(tracks) == 0:
-            continue
-        results[i].update(boxes=torch.as_tensor(tracks[:, :-1]))
-        if results[i].masks is not None:
-            idx = tracks[:, -1].tolist()
-            results[i].masks = results[i].masks[idx]
-    # '''
-    return results
+def on_predict_start(predictor):
+    tracker_list = []
+    for i in range(predictor.dataset.bs):
+        tracker = create_tracker('deepocsort', 'trackers/deepocsort/configs/deepocsort.yaml', Path('lmbn_n_duke.pt'), predictor.device, False)
+        print(tracker)
+        tracker_list.append(tracker)
+        if hasattr(tracker_list[i], 'model'):
+            if hasattr(tracker_list[i].model, 'warmup'):
+                tracker_list[i].model.warmup()
+    return tracker_list
+
 
 if __name__ == '__main__':
     
@@ -50,7 +43,7 @@ if __name__ == '__main__':
     imgsz = [640, 640]
     save_dir=False
     vid_stride = 1
-    verbose = False
+    verbose = True
     save = False
     save_txt = False
     show = True
@@ -58,85 +51,91 @@ if __name__ == '__main__':
     plotted_img = False
     augment = False
     
+    if source is None:
+        source = ROOT / 'assets' if is_git_dir() else 'https://ultralytics.com/images/bus.jpg'
+        LOGGER.warning(f"WARNING ⚠️ 'source' is missing. Using 'source={source}'.")
+    
     from ultralytics.yolo.engine.model import YOLO
     model = YOLO('yolov8s.pt')
-    model.overrides = model.overrides.copy()
-    model.overrides['conf'] = 0.25
+    overrides = model.overrides.copy()
+    model.predictor = TASK_MAP[model.task][3](overrides=overrides, _callbacks=model.callbacks)
+    predictor = model.predictor
+    if not predictor.model:
+        predictor.setup_model(model=model.model, verbose=False)
 
-    print(model.task)
-    print(model.predictor)
     # https://github.com/ultralytics/ultralytics/blob/main/ultralytics/yolo/engine/model.py
     #model.predictor.setup_model(model=model.model, verbose=False)
-    predictor = model.predictor
-    predictor.setup_model('yolov8s.pt')
-    predictor.setup_source(source if source is not None else source)
+    
+    predictor.setup_source(source if source is not None else predictor.args.source)
+    predictor.args.conf = 0.5
+    
     dataset = predictor.dataset
     model = predictor.model
     imgsz = check_imgsz(imgsz, stride=model.model.stride, min_dim=2)  # check image size
-    #dataset = load_inference_source(source=source, imgsz=imgsz, vid_stride=vid_stride)
     source_type = dataset.source_type
     preprocess = predictor.preprocess
     postprocess = predictor.postprocess
     run_callbacks = predictor.run_callbacks
     write_results = predictor.write_results
-    predictor.seen = 0
     
-    if not (dataset.mode == 'stream' or  # streams
-            len(dataset) > 1000 or  # images
-            any(getattr(dataset, 'video_flag', [False]))):  # videos
-        LOGGER.warning(STREAM_WARNING)
-    vid_path, vid_writer = [None] * dataset.bs, [None] * dataset.bs
-    
-    
-    seen, windows, batch, profilers = 0, [], None, (ops.Profile(), ops.Profile(), ops.Profile())
-    run_callbacks('on_predict_start')
+    # Check if save_dir/ label file exists
+    if predictor.args.save or predictor.args.save_txt:
+        (predictor.save_dir / 'labels' if predictor.args.save_txt else predictor.save_dir).mkdir(parents=True, exist_ok=True)
+    # Warmup model
+    if not predictor.done_warmup:
+        predictor.model.warmup(imgsz=(1 if predictor.model.pt or predictor.model.triton else predictor.dataset.bs, 3, *predictor.imgsz))
+        predictor.done_warmup = True
+    predictor.seen, predictor.windows, predictor.batch, predictor.profilers = 0, [], None, (ops.Profile(), ops.Profile(), ops.Profile())
+    predictor.add_callback('on_predict_start', on_predict_start)
+    tracker_list = run_callbacks('on_predict_start')
+    print(tracker_list)
     for batch in dataset:
         run_callbacks('on_predict_batch_start')
+        predictor.batch = batch
         path, im0s, vid_cap, s = batch
         visualize = increment_path(save_dir / Path(path[0]).stem,
                                     mkdir=True) if visualize and (not source_type.tensor) else False
 
         # Preprocess
-        with profilers[0]:
+        with predictor.profilers[0]:
             im = preprocess(im0s)
 
         # Inference
-        with profilers[1]:
+        with predictor.profilers[1]:
             preds = model(im, augment=augment, visualize=visualize)
-            print('preds', len(preds))
 
         # Postprocess
-        with profilers[2]:
-            results = postprocess(preds, im, im0s)
+        with predictor.profilers[2]:
+            predictor.results = postprocess(preds, im, im0s)
         run_callbacks('on_predict_postprocess_end')
-        print(len(results))
+        #print(predictor.results.boxes.shape)
         
         # Visualize, save, write results
         n = len(im0s)
         for i in range(n):
-            results[i].speed = {
-                'preprocess': profilers[0].dt * 1E3 / n,
-                'inference': profilers[1].dt * 1E3 / n,
-                'postprocess': profilers[2].dt * 1E3 / n}
+            predictor.results[i].speed = {
+                'preprocess': predictor.profilers[0].dt * 1E3 / n,
+                'inference': predictor.profilers[1].dt * 1E3 / n,
+                'postprocess': predictor.profilers[2].dt * 1E3 / n}
             if source_type.tensor:  # skip write, show and plot operations if input is raw tensor
                 continue
             p, im0 = path[i], im0s[i].copy()
             p = Path(p)
 
             if verbose or save or save_txt or show:
-                s += write_results(i, results, (p, im, im0))
+                s += write_results(i, predictor.results, (p, im, im0))
 
             if show and plotted_img is not None:
-                show(p)
+                predictor.show('blub')
 
             if save and plotted_img is not None:
                 save_preds(vid_cap, i, str(save_dir / p.name))
         run_callbacks('on_predict_batch_end')
-        [r for r in results]
+        [r for r in predictor.results]
 
         # Print time (inference-only)
         if verbose:
-            LOGGER.info(f'{s}{profilers[1].dt * 1E3:.1f}ms')
+            LOGGER.info(f'{s}{predictor.profilers[1].dt * 1E3:.1f}ms')
 
     # Release assets
     if isinstance(vid_writer[-1], cv2.VideoWriter):
