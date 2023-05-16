58620adbb723b1ff239a3bd023bcf179cbbab1e5
mikel.brostrom <mikel.brostrom@cevt.se>
2023-05-16 13:59:47 +0200

    fix yolo-model argument

--- track.py
+++ track.py
@@ -242,7 +242,7 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo-model', nargs='+', type=str, default=WEIGHTS / 'yolov8n.pt', help='model.pt path(s)')
+    parser.add_argument('--yolo-model', type=str, default=WEIGHTS / 'yolov8n.pt', help='model.pt path(s)')
     parser.add_argument('--reid-model', type=Path, default=WEIGHTS / 'mobilenetv2_x1_4_dukemtmcreid.pt')
     parser.add_argument('--tracking-method', type=str, default='deepocsort', help='deepocsort, botsort, strongsort, ocsort, bytetrack')
     parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob, 0 for webcam')  
@@ -250,32 +250,20 @@ def parse_opt():
     parser.add_argument('--conf', type=float, default=0.5, help='confidence threshold')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--show', action='store_true', help='display tracking video results')
-    # parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
+    parser.add_argument('--save', action='store_true', help='save video tracking results')
     # # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    # parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    # parser.add_argument('--augment', action='store_true', help='augmented inference')
-    # parser.add_argument('--visualize', action='store_true', help='visualize features')
-    # parser.add_argument('--update', action='store_true', help='update all models')
     parser.add_argument('--project', default=ROOT / 'runs' / 'track', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exists-ok', action='store_true', help='existing project/name ok, do not increment')
-    # parser.add_argument('--line-thickness', default=2, type=int, help='bounding box thickness (pixels)')
-    # parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    # parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    # parser.add_argument('--hide-class', default=False, action='store_true', help='hide IDs')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    # parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
     opt = parser.parse_args()
-    # opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    # opt.tracking_config = ROOT / 'trackers' / opt.tracking_method / 'configs' / (opt.tracking_method + '.yaml')
     print_args(vars(opt))
     return opt
 
 
 def main(opt):
-    #check_requirements(requirements=ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
     run(**vars(opt))
 
 
