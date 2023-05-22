94d2a875d4b1c2fbc8a2562effd86205b4239163
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-05-22 13:41:56 +0200

    match paths in val and track

--- examples/val.py
+++ examples/val.py
@@ -35,7 +35,7 @@ from track import run
 
 
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0].parents[0]  # examples absolute path
+ROOT = FILE.parents[0].parents[0]  # repo root absolute path
 EXAMPLES = FILE.parents[0]  # examples absolute path
 WEIGHTS = EXAMPLES / 'weights'
 
