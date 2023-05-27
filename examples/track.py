f894e2f3508feda0264d9da0610c94b3c9f17ab6
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-05-27 13:59:08 +0200

    move misc methods to utils

--- examples/track.py
+++ examples/track.py
@@ -17,6 +17,7 @@ from ultralytics.yolo.engine.results import Boxes
 from ultralytics.yolo.data.utils import VID_FORMATS
 
 from multi_yolo_backend import MultiYolo
+from utils import write_MOT_results
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0].parents[0]  # repo root absolute path
@@ -42,24 +43,6 @@ def on_predict_start(predictor):
             predictor.args.half
         )
         predictor.trackers.append(tracker)
-                
-                
-def write_MOT_results(txt_path, results, frame_idx, i):
-    nr_dets = len(results.boxes)
-    frame_idx = torch.full((1, 1), frame_idx + 1)
-    frame_idx = frame_idx.repeat(nr_dets, 1)
-    dont_care = torch.full((nr_dets, 3), -1)
-    i = torch.full((nr_dets, 1), i)
-    mot = torch.cat([
-        frame_idx,
-        results.boxes.id.unsqueeze(1).to('cpu'),
-        ops.xyxy2ltwh(results.boxes.xyxy).to('cpu'),
-        dont_care,
-        i
-    ], dim=1)
-
-    with open(str(txt_path) + '.txt', 'ab') as f:  # append binary mode
-        np.savetxt(f, mot.numpy(), fmt='%d')  # save as ints instead of scientific notation
 
 
 @torch.no_grad()
