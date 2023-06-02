e473bea7404786d45c69f1b88fbf82b50c201d75
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-06-02 10:05:52 +0200

    add boxmot abs path

--- boxmot/utils/__init__.py
+++ boxmot/utils/__init__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[2]  # root directory
+BOXMOT = ROOT / 'boxmot'
 EXAMPLES = ROOT / 'examples'
 WEIGHTS = ROOT / 'examples' / 'weights'
 REQUIREMENTS = ROOT / 'requirements.txt'
