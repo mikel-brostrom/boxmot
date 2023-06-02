e31c59df2500aeddabf3cc2a7c63c2e8277a9008
mikel.brostrom <mikel.brostrom@cevt.se>
2023-06-02 10:01:07 +0200

    path import from boxmot

--- boxmot/tracker_zoo.py
+++ boxmot/tracker_zoo.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import yaml
 from types import SimpleNamespace
+from boxmot.utils import BOXMOT
 
 
 def get_tracker_config(tracker_type):
     tracking_config = \
-        Path('./boxmot') /\
+        BOXMOT /\
         tracker_type /\
         'configs' /\
         (tracker_type + '.yaml')
@@ -13,7 +14,7 @@ def get_tracker_config(tracker_type):
     
 
 def create_tracker(tracker_type, tracker_config, reid_weights, device, half):
-    
+
     with open(tracker_config, "r") as f:
         cfg = yaml.load(f.read(), Loader=yaml.FullLoader)
     cfg = SimpleNamespace(**cfg)  # easier dict acces by dot, instead of ['']
