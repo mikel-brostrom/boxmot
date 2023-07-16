a6fe28cea61bb9c27ce159b2921dab2dad5a54d2
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-07-16 09:50:18 +0200

    Added pre-commit hooks, Makefile, reintroduced KF adapters

--- boxmot/trackers/strongsort/sort/track.py
+++ boxmot/trackers/strongsort/sort/track.py
@@ -1,7 +1,7 @@
 # vim: expandtab:ts=4:sw=4
 import cv2
 import numpy as np
-from ....motion.strongsort_kf import KalmanFilter
+from ....motion.adapters import StrongSortKalmanFilterAdapter
 from collections import deque
 
 
@@ -88,7 +88,7 @@ class Track:
         self._n_init = n_init
         self._max_age = max_age
 
-        self.kf = KalmanFilter()
+        self.kf = StrongSortKalmanFilterAdapter()
         self.mean, self.covariance = self.kf.initiate(detection)
         
         # Initializing trajectory queue
