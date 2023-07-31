55dedaf9d5e68a8fc2d2667ae4ddb160aabe3abd
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-07-31 12:25:56 +0200

    added clip vehicle models

--- boxmot/appearance/reid_model_factory.py
+++ boxmot/appearance/reid_model_factory.py
@@ -70,7 +70,9 @@ __trained_urls = {
     "lmbn_n_cuhk03_d.pt": lmbn_loc + "lmbn_n_cuhk03_d.pth",
     # clip
     "clip_market1501.pt": "https://drive.google.com/uc?id=1GnyAVeNOg3Yug1KBBWMKKbT2x43O5Ch7",
-    "clip_duke.pt": "https://drive.google.com/uc?id=1ldjSkj-7pXAWmx8on5x0EftlCaolU4dY"
+    "clip_duke.pt": "https://drive.google.com/uc?id=1ldjSkj-7pXAWmx8on5x0EftlCaolU4dY",
+    "clip_veri.pt": "https://drive.google.com/uc?id=1RyfHdOBI2pan_wIGSim5-l6cM4S2WN8e",
+    "clip_vehicleid.pt": "https://drive.google.com/uc?id=1RyfHdOBI2pan_wIGSim5-l6cM4S2WN8e"
 }
 
 
