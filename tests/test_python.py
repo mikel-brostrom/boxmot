7f470d31ed467882f1c848ee2b491a1e4ad46ed9
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-07-31 17:47:41 +0200

    add gsi test

--- tests/test_python.py
+++ tests/test_python.py
@@ -212,6 +212,8 @@ def test_gsi():
     tracking_results = np.array([
         [1, 1, 1475, 419, 75, 169, 0, 0, -1],
         [2, 1, 1475, 419, 75, 169, 0, 0, -1],
+        [4, 1, 1475, 419, 75, 169, 0, 0, -1],
     ])
     li = linear_interpolation(tracking_results, interval=20)
-    gaussian_smooth(li, tau=10)
+    gsi = gaussian_smooth(li, tau=10)
+    assert len(gsi) == 4
