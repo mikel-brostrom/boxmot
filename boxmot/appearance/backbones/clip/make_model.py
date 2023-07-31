c7d336fdebc454517ddad1dc8b14d506885536da
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-07-31 16:56:25 +0200

    delte to cuda

--- boxmot/appearance/backbones/clip/make_model.py
+++ boxmot/appearance/backbones/clip/make_model.py
@@ -65,7 +65,6 @@ class build_transformer(nn.Module):
         self.w_resolution = int((cfg.INPUT.SIZE_TRAIN[1] - 16) // cfg.MODEL.STRIDE_SIZE[1] + 1)
         self.vision_stride_size = cfg.MODEL.STRIDE_SIZE[0]
         clip_model = load_clip_to_cpu(self.model_name, self.h_resolution, self.w_resolution, self.vision_stride_size)
-        clip_model.to("cuda")
 
         self.image_encoder = clip_model.visual
 
