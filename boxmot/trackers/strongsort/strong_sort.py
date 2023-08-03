0f0d148b13709943052b4f108097662dcf21e790
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-08-03 22:07:26 +0200

    cleanup

--- boxmot/trackers/strongsort/strong_sort.py
+++ boxmot/trackers/strongsort/strong_sort.py
@@ -6,7 +6,7 @@ from boxmot.appearance.reid_multibackend import ReIDDetectMultiBackend
 from boxmot.trackers.strongsort.sort.detection import Detection
 from boxmot.trackers.strongsort.sort.tracker import Tracker
 from boxmot.utils.matching import NearestNeighborDistanceMetric
-from boxmot.utils.ops import xyxy2xywh
+from boxmot.utils.ops import tlwh2xyxy, xywh2tlwh, xyxy2xywh
 
 
 class StrongSORT(object):
@@ -18,28 +18,24 @@ class StrongSORT(object):
         max_dist=0.2,
         max_iou_dist=0.7,
         max_age=30,
-        max_unmatched_preds=7,
-        n_init=3,
+        n_init=0,
         nn_budget=100,
         mc_lambda=0.995,
         ema_alpha=0.9,
     ):
         self.model = ReIDDetectMultiBackend(
-            weights=model_weights, device=device, fp16=fp16
+            weights=model_weights,
+            device=device,
+            fp16=fp16
         )
-
-        self.max_dist = max_dist
-        metric = NearestNeighborDistanceMetric("cosine", self.max_dist, nn_budget)
         self.tracker = Tracker(
-            metric,
+            metric=NearestNeighborDistanceMetric("cosine", max_dist, nn_budget),
             max_iou_dist=max_iou_dist,
             max_age=max_age,
             n_init=n_init,
-            max_unmatched_preds=max_unmatched_preds,
             mc_lambda=mc_lambda,
             ema_alpha=ema_alpha,
         )
-        self.previous_img = None
 
     def update(self, dets, img):
         assert isinstance(
@@ -63,12 +59,11 @@ class StrongSORT(object):
         clss = dets[:, 5]
 
         xywhs = xyxy2xywh(xyxys)
-        confs = confs
         self.height, self.width = img.shape[:2]
 
         # generate detections
-        features = self._get_features(xywhs, img)
-        bbox_tlwh = self._xywh_to_tlwh(xywhs)
+        features = self._get_features(xyxys, img)
+        bbox_tlwh = xywh2tlwh(xywhs)
         detections = [
             Detection(bbox_tlwh[i], conf, features[i]) for i, conf in enumerate(confs)
         ]
@@ -83,8 +78,8 @@ class StrongSORT(object):
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue
 
-            box = track.to_tlwh()
-            x1, y1, x2, y2 = self._tlwh_to_xyxy(box)
+            tlwh = track.to_tlwh()
+            x1, y1, x2, y2 = tlwh2xyxy(tlwh)
 
             track_id = track.track_id
             class_id = track.class_id
@@ -96,60 +91,14 @@ class StrongSORT(object):
         outputs = np.asarray(outputs)
         return outputs
 
-    """
-    TODO:
-        Convert bbox from xc_yc_w_h to xtl_ytl_w_h
-    Thanks JieChen91@github.com for reporting this bug!
-    """
-
-    @staticmethod
-    def _xywh_to_tlwh(bbox_xywh):
-        if isinstance(bbox_xywh, np.ndarray):
-            bbox_tlwh = bbox_xywh.copy()
-        elif isinstance(bbox_xywh, torch.Tensor):
-            bbox_tlwh = bbox_xywh.clone()
-        bbox_tlwh[:, 0] = bbox_xywh[:, 0] - bbox_xywh[:, 2] / 2.0
-        bbox_tlwh[:, 1] = bbox_xywh[:, 1] - bbox_xywh[:, 3] / 2.0
-        return bbox_tlwh
-
-    def _xywh_to_xyxy(self, bbox_xywh):
-        x, y, w, h = bbox_xywh
-        x1 = max(int(x - w / 2), 0)
-        x2 = min(int(x + w / 2), self.width - 1)
-        y1 = max(int(y - h / 2), 0)
-        y2 = min(int(y + h / 2), self.height - 1)
-        return x1, y1, x2, y2
-
-    def _tlwh_to_xyxy(self, bbox_tlwh):
-        """
-        TODO:
-            Convert bbox from xtl_ytl_w_h to xc_yc_w_h
-        Thanks JieChen91@github.com for reporting this bug!
-        """
-        x, y, w, h = bbox_tlwh
-        x1 = max(int(x), 0)
-        x2 = min(int(x + w), self.width - 1)
-        y1 = max(int(y), 0)
-        y2 = min(int(y + h), self.height - 1)
-        return x1, y1, x2, y2
-
     def increment_ages(self):
         self.tracker.increment_ages()
 
-    def _xyxy_to_tlwh(self, bbox_xyxy):
-        x1, y1, x2, y2 = bbox_xyxy
-
-        t = x1
-        l = y1
-        w = int(x2 - x1)
-        h = int(y2 - y1)
-        return t, l, w, h
-
     @torch.no_grad()
-    def _get_features(self, bbox_xywh, img):
+    def _get_features(self, xyxys, img):
         im_crops = []
-        for box in bbox_xywh:
-            x1, y1, x2, y2 = self._xywh_to_xyxy(box)
+        for box in xyxys:
+            x1, y1, x2, y2 = box.astype('int')
             im = img[y1:y2, x1:x2]
             im_crops.append(im)
         if im_crops:
