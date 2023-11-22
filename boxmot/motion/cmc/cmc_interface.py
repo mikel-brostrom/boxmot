24285f56e66b159fb2a937acb1b8af7871ef6404
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2023-11-22 13:47:01 +0100

    move preprocess to cmc interface class

--- boxmot/motion/cmc/cmc_interface.py
+++ boxmot/motion/cmc/cmc_interface.py
@@ -21,3 +21,21 @@ class CMCInterface(ABC):
                 mask[tlbr[1]:tlbr[3], tlbr[0]:tlbr[2]] = 0
 
         return mask
+
+    def preprocess(self, img):
+
+        # bgr2gray
+        if self.grayscale:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # resize
+        if self.scale is not None:
+            img = cv2.resize(
+                img,
+                (0, 0),
+                fx=self.scale,
+                fy=self.scale,
+                interpolation=cv2.INTER_LINEAR
+            )
+
+        return img
