9bc8543b191317dc3042134a0d8c19feea624975
mikel.brostrom <mikel.brostrom@cevt.se>
2023-11-24 10:41:10 +0100

    abc

--- examples/detectors/yolo_interface.py
+++ examples/detectors/yolo_interface.py
@@ -11,7 +11,7 @@ from abc import ABC, abstractmethod
 class YoloInterface(ABC):
 
     @abstractmethod
-    def inference(self, im):
+    def __call__(self, im):
         pass
 
     @abstractmethod
