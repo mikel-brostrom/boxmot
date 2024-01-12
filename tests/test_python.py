c3e429f232da66198fa592882a4bba2f894b2efa
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-01-12 22:16:22 +0100

    add centroid asso test

--- tests/test_python.py
+++ tests/test_python.py
@@ -56,7 +56,8 @@ def test_deepocsort_output():
         reid_weights=WEIGHTS / 'mobilenetv2_x1_4_dukemtmcreid.pt',
         device='cpu',
         half=False,
-        per_class=False
+        per_class=False,
+        asso_func="centroid"
     )
     rgb = np.random.randint(255, size=(640, 640, 3), dtype=np.uint8)
     det = np.array([[144, 212, 578, 480, 0.82, 0],
