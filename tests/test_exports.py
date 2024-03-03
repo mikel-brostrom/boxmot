4e59f63231b13af5dca0cab2d3ace3ae7732b522
mikel.brostrom <mikel.brostrom@cevt.se>
2024-03-03 13:02:23 +0100

    added reid model download now that it is a separate job

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -8,32 +8,29 @@ from boxmot.appearance.reid_model_factory import (get_model_name,
                                                   load_pretrained_weights)
 from boxmot.utils import WEIGHTS
 
+
+from boxmot.appearance.reid_multibackend import ReIDDetectMultiBackend
+
+
+WEIGHTS.mkdir(parents=False, exist_ok=True)
 PT_WEIGHTS = WEIGHTS / 'osnet_x0_25_msmt17.pt'
 ONNX_WEIGHTS = WEIGHTS / 'osnet_x0_25_msmt17.onnx'
 
+ReIDDetectMultiBackend(
+    weights=PT_WEIGHTS,
+    device='cpu',
+    fp16=False
+)
 
 im = torch.zeros(1, 3, 256, 128)
-
 model = build_model(
     get_model_name(PT_WEIGHTS),
     num_classes=1,
     pretrained=False,
     use_gpu=False,
 ).to('cpu')
-
 load_pretrained_weights(model, PT_WEIGHTS)
 model.eval()
-
-
-def test_export_torchscript():
-    f = export_torchscript(
-        model,
-        im,
-        PT_WEIGHTS,
-        True,
-    )
-    assert f is not None
-
 
 def test_export_onnx():
     f = export_onnx(
