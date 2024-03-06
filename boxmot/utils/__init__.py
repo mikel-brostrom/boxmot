85118f8a40778c4fcb70e9b85b62c3033b9aa1a0
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-03-06 18:09:53 +0100

    added class decorator

--- boxmot/utils/__init__.py
+++ boxmot/utils/__init__.py
@@ -23,41 +23,46 @@ logger.add(sys.stderr, colorize=True, level="INFO")
 
 class PerClassDecorator:
     def __init__(self, method):
-        self.update = method
+        # Store the method that will be decorated
+        self.update_method = method
 
     def __get__(self, instance, owner):
+        # This makes PerClassDecorator a non-data descriptor that binds the method to the instance
         def wrapper(*args, **kwargs):
-            modified_args = list(args)
-            dets = modified_args[0]
-            im = modified_args[1]
-
-            # input one class of detections at a time in order to not mix them up
-            if instance.per_class is True and dets.size != 0:
-                dets_dict = {
-                    class_id: np.array([det for det in dets if det[5] == class_id])
-                    for class_id in set(det[5] for det in dets)
+            # Unpack arguments for clarity
+            detections, image = args
+            
+            if instance.per_class is True and detections.size != 0:
+                # Organize detections by class ID for per-class processing
+                detections_by_class = {
+                    class_id: np.array([det for det in detections if det[5] == class_id])
+                    for class_id in set(det[5] for det in detections)
                 }
-                # get unique classes in predictions
-                detected_classes = set(dets_dict.keys())
-                # get unque classes with active trackers
-                active_classes = set([tracker.cls for tracker in instance.active_tracks])
-                # get tracks that are both active and in the current detections
+
+                # Detect classes in the current frame and active trackers
+                detected_classes = set(detections_by_class.keys())
+                active_classes = set(tracker.cls for tracker in instance.active_tracks)
+                
+                # Determine relevant classes for processing
                 relevant_classes = active_classes.union(detected_classes)
 
-                mc_dets = np.empty(shape=(0, 8))
+                # Initialize an array to store modified detections
+                modified_detections = np.empty(shape=(0, 8))
                 for class_id in relevant_classes:
-                    modified_args[0] = np.array(
-                        dets_dict.get(int(class_id), np.empty((0, 6)))
-                    )
-                    logger.debug(
-                        f"Feeding class {int(class_id)}: {modified_args[0].shape}"
-                    )
-                    dets = self.update(instance, modified_args[0], im)
-                    if dets.size != 0:
-                        mc_dets = np.append(mc_dets, dets, axis=0)
-                logger.debug(f"Per class updates output: {mc_dets.shape}")
+                    # Process detections class-by-class
+                    current_class_detections = detections_by_class.get(int(class_id), np.empty((0, 6)))
+                    logger.debug(f"Processing class {int(class_id)}: {current_class_detections.shape}")
+                    
+                    # Update detections using the decorated method
+                    updated_dets = self.update_method(instance, current_class_detections, image)
+                    if updated_dets.size != 0:
+                        modified_detections = np.append(modified_detections, updated_dets, axis=0)
+
+                logger.debug(f"Per-class update result: {modified_detections.shape}")
             else:
-                mc_dets = self.update(instance, dets, im)
-            return mc_dets
+                # Process all detections at once if per_class is False or detections are empty
+                modified_detections = self.update_method(instance, detections, image)
+
+            return modified_detections
 
         return wrapper
