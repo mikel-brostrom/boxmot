02e11a7304fc3f39e2e3f4f219d40e3b71c6b062
Mikel Broström <yolov5.deepsort.pytorch@gmail.com>
2024-03-12 21:40:21 +0100

    add fp16 test

--- tests/unit/test_cuda.py
+++ tests/unit/test_cuda.py
@@ -8,7 +8,7 @@ from boxmot.utils import ROOT
 from boxmot.appearance.reid_multibackend import ReIDDetectMultiBackend
 
 REID_MODELS = [
-    Path('osnet_x0_25_msmt17.pt'),
+    Path('resnet50_market1501.pt'),
 ]
 
 
@@ -25,3 +25,22 @@ def test_reidbackend_device(reid_model):
         assert next(r.model.parameters()).is_cuda
     else:
         assert next(r.model.parameters()).device.type == 'cpu'
+
+
+@pytest.mark.parametrize("reid_model", REID_MODELS)
+def test_reidbackend_half(reid_model):
+
+    half = True if torch.cuda.is_available() else False
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    r = ReIDDetectMultiBackend(
+        weights=reid_model,
+        device=device,
+        fp16=half
+    )
+
+    if device is 'cpu':
+        expected_dtype = torch.float32
+    else:
+        expected_dtype = torch.float16
+    actual_dtype = next(r.model.parameters()).dtype
+    assert actual_dtype == expected_dtype
