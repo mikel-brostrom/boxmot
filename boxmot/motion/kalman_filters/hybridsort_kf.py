17b960f14d8f2cdff7fa4f6e7e8fc2a9f2d0b8e1
Mikel Broström <yolov5.deepsort.pytorch@gmail.com>
2024-06-13 17:21:50 +0200

    fix kfs

--- boxmot/motion/kalman_filters/hybridsort_kf.py
+++ boxmot/motion/kalman_filters/hybridsort_kf.py
@@ -281,7 +281,7 @@ class KalmanFilter(object):
        https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python
     """
 
-    def __init__(self, dim_x, dim_z, dim_u=0):
+    def __init__(self, dim_x, dim_z, dim_u=0, max_obs=50):
         if dim_x < 1:
             raise ValueError('dim_x must be 1 or greater')
         if dim_z < 1:
@@ -329,7 +329,8 @@ class KalmanFilter(object):
         self._mahalanobis = None
 
         # keep all observations 
-        self.history_obs = deque([], maxlen=50)
+        self.max_obs = max_obs
+        self.history_obs = deque([], maxlen=self.max_obs)
 
         self.inv = np.linalg.inv
 
@@ -393,7 +394,7 @@ class KalmanFilter(object):
             new_history = deepcopy(list(self.history_obs))
             self.__dict__ = self.attr_saved
             # self.history_obs = new_history 
-            self.history_obs = deque(list(self.history_obs)[:-1], maxlen=50)
+            self.history_obs = deque(list(self.history_obs)[:-1], maxlen=self.max_obs)
             occur = [int(d is None) for d in new_history]
             indices = np.where(np.array(occur)==0)[0]
             index1 = indices[-2]
