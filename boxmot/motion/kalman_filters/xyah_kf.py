cf0b36a73eb6e4e84d37b63c98a0bb05ba699b38
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-06-20 15:11:50 +0200

    added comments

--- boxmot/motion/kalman_filters/xyah_kf.py
+++ boxmot/motion/kalman_filters/xyah_kf.py
@@ -13,18 +13,24 @@ class KalmanFilterXYAH(BaseKalmanFilter):
         super().__init__(ndim=4)
 
     def _get_initial_covariance_std(self, measurement: np.ndarray) -> np.ndarray:
+        # initial uncertainty in the aspect ratio is very low,
+        # suggesting that it is not expected to vary significantly.
         return [
-            2 * self._std_weight_position * measurement[3],
-            2 * self._std_weight_position * measurement[3],
-            1e-2,
-            2 * self._std_weight_position * measurement[3],
-            10 * self._std_weight_velocity * measurement[3],
-            10 * self._std_weight_velocity * measurement[3],
-            1e-5,
-            10 * self._std_weight_velocity * measurement[3]
+            2 * self._std_weight_position * measurement[3],     # x
+            2 * self._std_weight_position * measurement[3],     # y
+            1e-2,                                               # a (aspect ratio)
+            2 * self._std_weight_position * measurement[3],     # H
+            10 * self._std_weight_velocity * measurement[3],    # vx
+            10 * self._std_weight_velocity * measurement[3],    # vy
+            1e-5,                                               # va (aspect ration vel)
+            10 * self._std_weight_velocity * measurement[3]     # vh
         ]
 
     def _get_process_noise_std(self, mean: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        # very small process noise standard deviation assigned to the
+        # aspect ratio state and its velocity. Suggests
+        # that the aspect ratio is expected to remain relatively constant
+        # over time.
         std_pos = [
             self._std_weight_position * mean[3],
             self._std_weight_position * mean[3],
@@ -40,6 +46,9 @@ class KalmanFilterXYAH(BaseKalmanFilter):
         return std_pos, std_vel
 
     def _get_measurement_noise_std(self, mean: np.ndarray, confidence: float) -> np.ndarray:
+        # small measurement noise standard deviation for
+        # aspect ratio state, indicating low expected measurement noise in
+        # the aspect ratio.
         std_noise = [
             self._std_weight_position * mean[3],
             self._std_weight_position * mean[3],
