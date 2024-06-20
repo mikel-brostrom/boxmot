1fd81656205f67faafca67d816eba9b8cfe5829e
Mikel Broström <yolov5.deepsort.pytorch@gmail.com>
2024-06-20 08:37:37 +0200

    clean up xyah and xywh kfs

--- boxmot/trackers/strongsort/sort/track.py
+++ boxmot/trackers/strongsort/sort/track.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from boxmot.motion.kalman_filters.strongsort_kf import KalmanFilter
+from boxmot.motion.kalman_filters.xyah_kf import KalmanFilterXYAH
 
 
 class TrackState:
@@ -94,7 +94,7 @@ class Track:
         self._n_init = n_init
         self._max_age = max_age
 
-        self.kf = KalmanFilter()
+        self.kf = KalmanFilterXYAH()
         self.mean, self.covariance = self.kf.initiate(self.bbox)
 
     def to_tlwh(self):
