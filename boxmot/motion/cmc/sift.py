90374dc2380b6d69002f0e951578c87302074896
Mike <yolov5.deepsort.pytorch@gmail.com>
2024-06-21 00:13:01 +0200

    Revert "added type hints"

--- boxmot/motion/cmc/sift.py
+++ boxmot/motion/cmc/sift.py
@@ -6,11 +6,11 @@ import time
 import cv2
 import numpy as np
 
-from boxmot.motion.cmc.base_cmc import BaseCMC
+from boxmot.motion.cmc.cmc_interface import CMCInterface
 from boxmot.utils import BOXMOT
 
 
-class SIFT(BaseCMC):
+class SIFT(CMCInterface):
 
     def __init__(
         self,
