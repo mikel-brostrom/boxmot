204ad3b856511cf97b7adbc6f094979dde9ee434
edblu1 <edgarblumenthal@gmx.de>
2024-07-02 11:11:29 +0200

    Merge branch 'master' into Q-scaling-rebase

---
+++ boxmot/motion/kalman_filters/xywh_kf.py
@@ -0,0 +1,64 @@
+import numpy as np
+from typing import Tuple
+from boxmot.motion.kalman_filters.base_kalman_filter import BaseKalmanFilter
+
+
+class KalmanFilterXYWH(BaseKalmanFilter):
+    """
+    A Kalman filter for tracking bounding boxes in image space with state space:
+        x, y, w, h, vx, vy, vw, vh
+    """
+
+    def __init__(self):
+        super().__init__(ndim=4)
+
+    def _get_initial_covariance_std(self, measurement: np.ndarray) -> np.ndarray:
+        return [
+            2 * self._std_weight_position * measurement[2],
+            2 * self._std_weight_position * measurement[3],
+            2 * self._std_weight_position * measurement[2],
+            2 * self._std_weight_position * measurement[3],
+            10 * self._std_weight_velocity * measurement[2],
+            10 * self._std_weight_velocity * measurement[3],
+            10 * self._std_weight_velocity * measurement[2],
+            10 * self._std_weight_velocity * measurement[3]
+        ]
+
+    def _get_process_noise_std(self, mean: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        std_pos = [
+            self._std_weight_position * mean[2],
+            self._std_weight_position * mean[3],
+            self._std_weight_position * mean[2],
+            self._std_weight_position * mean[3]
+        ]
+        std_vel = [
+            self._std_weight_velocity * mean[2],
+            self._std_weight_velocity * mean[3],
+            self._std_weight_velocity * mean[2],
+            self._std_weight_velocity * mean[3]
+        ]
+        return std_pos, std_vel
+
+    def _get_measurement_noise_std(self, mean: np.ndarray, confidence: float) -> np.ndarray:
+        std_noise = [
+            self._std_weight_position * mean[2],
+            self._std_weight_position * mean[3],
+            self._std_weight_position * mean[2],
+            self._std_weight_position * mean[3]
+        ]
+        return std_noise
+    
+    def _get_multi_process_noise_std(self, mean: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        std_pos = [
+            self._std_weight_position * mean[:, 2],
+            self._std_weight_position * mean[:, 3],
+            self._std_weight_position * mean[:, 2],
+            self._std_weight_position * mean[:, 3]
+        ]
+        std_vel = [
+            self._std_weight_velocity * mean[:, 2],
+            self._std_weight_velocity * mean[:, 3],
+            self._std_weight_velocity * mean[:, 2],
+            self._std_weight_velocity * mean[:, 3]
+        ]
+        return std_pos, std_vel
