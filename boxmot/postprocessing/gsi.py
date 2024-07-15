bc92a3c05087c019a0fe5808f50f093fcb18c540
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-07-15 10:14:11 +0200

    refactor and docstrings

--- boxmot/postprocessing/gsi.py
+++ boxmot/postprocessing/gsi.py
@@ -1,15 +1,22 @@
 # Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
 
 from pathlib import Path
-
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor as GPR
 from sklearn.gaussian_process.kernels import RBF
-
 from boxmot.utils import logger as LOGGER
 
-
 def linear_interpolation(input_, interval):
+    """
+    Perform linear interpolation on the input array.
+
+    Args:
+        input_ (np.ndarray): Input array with shape (n, m) where n is the number of entries and m is the number of features.
+        interval (int): Maximum interval for interpolation.
+
+    Returns:
+        np.ndarray: Interpolated array.
+    """
     input_ = input_[np.lexsort([input_[:, 0], input_[:, 1]])]
     output_ = input_.copy()
 
@@ -29,52 +36,62 @@ def linear_interpolation(input_, interval):
     output_ = output_[np.lexsort([output_[:, 0], output_[:, 1]])]
     return output_
 
-
 def gaussian_smooth(input_, tau):
-    output_ = list()
-    print('input_', input_)
+    """
+    Apply Gaussian smoothing to the input array.
+
+    Args:
+        input_ (np.ndarray): Input array with shape (n, m) where n is the number of entries and m is the number of features.
+        tau (float): Smoothing parameter.
+
+    Returns:
+        list: Smoothed array.
+    """
+    output_ = []
     ids = set(input_[:, 1])
-    for i, id_ in enumerate(ids):
+    for id_ in ids:
         tracks = input_[input_[:, 1] == id_]
-        print('tracks', tracks)
         len_scale = np.clip(tau * np.log(tau ** 3 / len(tracks)), tau ** -1, tau ** 2)
         gpr = GPR(RBF(len_scale, 'fixed'))
+        
         t = tracks[:, 0].reshape(-1, 1)
-        x = tracks[:, 2].reshape(-1, 1)
-        y = tracks[:, 3].reshape(-1, 1)
-        w = tracks[:, 4].reshape(-1, 1)
-        h = tracks[:, 5].reshape(-1, 1)
-        gpr.fit(t, x)
-        xx = gpr.predict(t)
-        gpr.fit(t, y)
-        yy = gpr.predict(t)
-        gpr.fit(t, w)
-        ww = gpr.predict(t)
-        gpr.fit(t, h)
-        hh = gpr.predict(t)
-        # frame count, id, x, y, w, h, conf, cls, -1 (don't care)
-        output_.extend([
-            [t[j, 0], id_, xx[j], yy[j], ww[j], hh[j], tracks[j, 6], tracks[j, 7], -1] for j in range(len(t))
-        ])
+        features = [tracks[:, i].reshape(-1, 1) for i in range(2, 6)]
+        smoothed_features = []
+
+        for feature in features:
+            gpr.fit(t, feature)
+            smoothed_features.append(gpr.predict(t))
+
+        for i in range(len(t)):
+            output_.append([t[i, 0], id_, *[sf[i, 0] for sf in smoothed_features], tracks[i, 6], tracks[i, 7], -1])
+
     return output_
 
-
 def gsi(mot_results_folder=Path('examples/runs/val/exp87/labels'), interval=20, tau=10):
+    """
+    Apply Gaussian Smoothed Interpolation (GSI) to tracking results.
+
+    Args:
+        mot_results_folder (Path): Path to the folder containing tracking result files.
+        interval (int): Maximum interval for interpolation.
+        tau (float): Smoothing parameter.
+    """
     tracking_results_files = mot_results_folder.glob('MOT*FRCNN.txt')
     for p in tracking_results_files:
         LOGGER.info(f"Applying gaussian smoothed interpolation (GSI) to: {p}")
         tracking_results = np.loadtxt(p, dtype=int, delimiter=' ')
         if tracking_results.size != 0:
             li = linear_interpolation(tracking_results, interval)
-            gsi = gaussian_smooth(li, tau)
-            np.savetxt(p, gsi, fmt='%d %d %d %d %d %d %d %d %d')
+            gsi_results = gaussian_smooth(li, tau)
+            np.savetxt(p, gsi_results, fmt='%d %d %d %d %d %d %d %d %d')
         else:
-            print('No tracking result in {p}. Skipping...')
-
+            LOGGER.info(f'No tracking result in {p}. Skipping...')
 
 def main():
+    """
+    Main function to apply Gaussian Smoothed Interpolation (GSI) to tracking results.
+    """
     gsi()
-
 
 if __name__ == "__main__":
     main()
