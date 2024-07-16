29e848fa8fcb54a665deaabec456a773c03deccf
Mikel Broström <mikel.brostrom@cevt.se>
2024-07-16 22:33:55 +0200

    apply export decorator

--- boxmot/appearance/exporters/tflite_exporter.py
+++ boxmot/appearance/exporters/tflite_exporter.py
@@ -4,13 +4,17 @@ from boxmot.utils import logger as LOGGER
 
 
 class TFLiteExporter(BaseExporter):
+    required_packages = (
+        "onnx2tf>=1.15.4", 
+        "tensorflow", 
+        "onnx_graphsurgeon>=0.3.26", 
+        "sng4onnx>=1.0.1"
+    )
+    cmds = '--extra-index-url https://pypi.ngc.nvidia.com'
+    
     def export(self):
-        self.checker.check_packages(
-            ("onnx2tf>=1.15.4", "tensorflow", "onnx_graphsurgeon>=0.3.26", "sng4onnx>=1.0.1"),
-            cmds='--extra-index-url https://pypi.ngc.nvidia.com'
-        )
-        import onnx2tf
 
+        import onnx2tf
         LOGGER.info(f"\nStarting {self.file} export with onnx2tf {onnx2tf.__version__}")
         f = str(self.file).replace(".onnx", f"_saved_model{os.sep}")
         cmd = f"onnx2tf -i {self.file} -o {f} -osd -coion --non_verbose"
