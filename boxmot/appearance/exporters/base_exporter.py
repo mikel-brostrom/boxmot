6f22c30fcbaa53d34f7daf4f5cffced1cf297ad6
Mikel Broström <mikel.brostrom@cevt.se>
2024-07-16 23:46:09 +0200

    delete decorator in class

--- boxmot/appearance/exporters/base_exporter.py
+++ boxmot/appearance/exporters/base_exporter.py
@@ -50,19 +50,6 @@ class BaseExporter:
     def export(self):
         raise NotImplementedError("Export method must be implemented in subclasses.")
     
-    def export_decorator(self, export_func):
-        def wrapper(self, *args, **kwargs):
-            try:
-                LOGGER.info(f"\nStarting export with {self.__class__.__name__}...")
-                result = export_func(self, *args, **kwargs)
-                if result:
-                    LOGGER.info(f"Export success, saved as {result} ({self.file_size(result):.1f} MB)")
-                return result
-            except Exception as e:
-                LOGGER.error(f"Export failure: {e}")
-                return None
-        return wrapper
-    
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         if 'export' in cls.__dict__:
