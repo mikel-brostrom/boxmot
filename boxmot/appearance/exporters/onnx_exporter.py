ecf91b4cfe9794c8c04263ef959ce86227c8d773
Mikel Broström <mikel.brostrom@cevt.se>
2024-07-16 23:51:36 +0200

    fix tflite export

--- boxmot/appearance/exporters/onnx_exporter.py
+++ boxmot/appearance/exporters/onnx_exporter.py
@@ -5,7 +5,7 @@ from boxmot.utils import logger as LOGGER
 
 
 class ONNXExporter(BaseExporter):
-    required_packages = ("onnx=>1.14.0",)
+    required_packages = ("onnx=>1.16.1",)
     
     def export(self):
 
