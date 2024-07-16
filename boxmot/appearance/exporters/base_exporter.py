dfc810fda353c47a0f035fc2b72b0b164239d52d
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-07-16 22:42:51 +0200

    apply export decorator

--- boxmot/appearance/exporters/base_exporter.py
+++ boxmot/appearance/exporters/base_exporter.py
@@ -9,7 +9,10 @@ def export_decorator(export_func):
     def wrapper(self, *args, **kwargs):
         try:
             if hasattr(self, 'required_packages'):
-                self.checker.check_packages(self.required_packages, cmd=self.cmd)
+                if hasattr(self, 'cmd'):
+                    self.checker.check_packages(self.required_packages, cmd=self.cmd)
+                else:
+                    self.checker.check_packages(self.required_packages)
                 
             LOGGER.info(f"\nStarting export with {self.__class__.__name__}...")
             result = export_func(self, *args, **kwargs)
