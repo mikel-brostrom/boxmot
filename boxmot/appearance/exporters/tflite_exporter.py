c747a587e4a57bafd40b9d10679404f61f1c987c
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-07-17 00:35:19 +0200

    check folder generatedä

--- boxmot/appearance/exporters/tflite_exporter.py
+++ boxmot/appearance/exporters/tflite_exporter.py
@@ -19,14 +19,15 @@ class TFLiteExporter(BaseExporter):
 
         import onnx2tf
         LOGGER.info(f"\nStarting {self.file} export with onnx2tf {onnx2tf.__version__}")
-        f = str(self.file).replace(".onnx", f"_saved_model{os.sep}")
+        input_onnx_file_path = str(self.file.with_suffix('.onnx'))
+        output_folder_path = str(self.file).replace(".onnx", f"_saved_model{os.sep}")
         onnx2tf.convert(
-            input_onnx_file_path="/home/runner/work/boxmot/boxmot/tracking/weights/osnet_x0_25_msmt17.onnx",
-            output_folder_path="/home/runner/work/boxmot/boxmot/tracking/weights/osnet_x0_25_msmt17_saved_model/",
+            input_onnx_file_path=input_onnx_file_path,
+            output_folder_path=output_folder_path,
             not_use_onnxsim=True,
             verbosity=True,
             #output_integer_quantized_tflite=self.args.int8,
             quant_type="per-tensor",  # "per-tensor" (faster) or "per-channel" (slower but more accurate)
             #custom_input_op_name_np_data_path=np_data,
         )
-        return f
+        return output_folder_path
