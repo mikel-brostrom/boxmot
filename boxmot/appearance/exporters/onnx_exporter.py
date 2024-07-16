a41770dc49f9ebffab427ac1b0165f28ce4e9dfa
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-07-16 22:06:05 +0200

    apply export decorator

--- boxmot/appearance/exporters/onnx_exporter.py
+++ boxmot/appearance/exporters/onnx_exporter.py
@@ -6,36 +6,32 @@ from boxmot.utils import logger as LOGGER
 
 class ONNXExporter(BaseExporter):
     def export(self):
-        try:
-            self.checker.check_packages(("onnx==1.14.0",))
-            f = self.file.with_suffix(".onnx")
-            LOGGER.info(f"\nStarting export with onnx {onnx.__version__}...")
 
-            dynamic = {"images": {0: "batch"}, "output": {0: "batch"}} if self.dynamic else None
+        self.checker.check_packages(("onnx==1.14.0",))
+        f = self.file.with_suffix(".onnx")
+        LOGGER.info(f"\nStarting export with onnx {onnx.__version__}...")
 
-            torch.onnx.export(
-                self.model.cpu() if self.dynamic else self.model,
-                self.im.cpu() if self.dynamic else self.im,
-                f,
-                verbose=False,
-                opset_version=12,
-                do_constant_folding=True,
-                input_names=["images"],
-                output_names=["output"],
-                dynamic_axes=dynamic,
-            )
+        dynamic = {"images": {0: "batch"}, "output": {0: "batch"}} if self.dynamic else None
 
-            model_onnx = onnx.load(f)
-            onnx.checker.check_model(model_onnx)
-            onnx.save(model_onnx, f)
+        torch.onnx.export(
+            self.model.cpu() if self.dynamic else self.model,
+            self.im.cpu() if self.dynamic else self.im,
+            f,
+            verbose=False,
+            opset_version=12,
+            do_constant_folding=True,
+            input_names=["images"],
+            output_names=["output"],
+            dynamic_axes=dynamic,
+        )
 
-            if self.simplify:
-                self.simplify_model(model_onnx, f)
+        model_onnx = onnx.load(f)
+        onnx.checker.check_model(model_onnx)
+        onnx.save(model_onnx, f)
 
-            LOGGER.info(f"Export success, saved as {f} ({self.file_size(f):.1f} MB)")
-            return f
-        except Exception as e:
-            LOGGER.error(f"Export failure: {e}")
+        if self.simplify:
+            self.simplify_model(model_onnx, f)
+
 
     def simplify_model(self, model_onnx, f):
         try:
