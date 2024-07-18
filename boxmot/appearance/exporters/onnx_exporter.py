79a04bd4e01fbb585597b394b29884265f311bfe
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-07-18 12:05:50 +0200

    => not >=

--- boxmot/appearance/exporters/onnx_exporter.py
+++ boxmot/appearance/exporters/onnx_exporter.py
@@ -5,7 +5,7 @@ from boxmot.utils import logger as LOGGER
 
 
 class ONNXExporter(BaseExporter):
-    required_packages = ("onnx>=x1.16.1",)
+    required_packages = ("onnx>=1.16.1",)
     
     def export(self):
 
