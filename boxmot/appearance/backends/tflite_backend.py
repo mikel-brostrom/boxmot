fe8c2c258a0dc8fae03bd077e3f12411a6f7e18a
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-07-19 15:13:54 +0200

    fix

--- boxmot/appearance/backends/tflite_backend.py
+++ boxmot/appearance/backends/tflite_backend.py
@@ -10,32 +10,64 @@ checker = RequirementsChecker()
 
 
 class TFLiteBackend(BaseModelBackend):
+    """
+    A class to handle TensorFlow Lite model inference with dynamic batch size support.
 
-    def __init__(self, weights, device, half):
+    Attributes:
+        nhwc (bool): A flag indicating the order of dimensions.
+        half (bool): A flag to indicate if half precision is used.
+        interpreter (tf.lite.Interpreter): The TensorFlow Lite interpreter.
+        current_allocated_batch_size (int): The current batch size allocated in the interpreter.
+    """
+
+    def __init__(self, weights: str, device: str, half: bool):
+        """
+        Initializes the TFLiteBackend with given weights, device, and precision flag.
+
+        Args:
+            weights (str): Path to the TFLite model file.
+            device (str): Device type (e.g., 'cpu', 'gpu').
+            half (bool): Flag to indicate if half precision is used.
+        """
         super().__init__(weights, device, half)
         self.nhwc = False
         self.half = half
-        self.interpreter = None
-        self.current_allocated_batch_size = None
+        self.interpreter: tf.lite.Interpreter = None
+        self.current_allocated_batch_size: int = None
 
-    def load_model(self, w):
+    def load_model(self, w: str) -> None:
+        """
+        Loads the TensorFlow Lite model and initializes the interpreter.
+
+        Args:
+            w (str): Path to the TFLite model file.
+        """
         checker.check_packages(("tensorflow",))
 
         LOGGER.info(f"Loading {w} for TensorFlow Lite inference...")
-        
+
         try:
             import tensorflow as tf
             self.interpreter = tf.lite.Interpreter(model_path=str(w))
         except Exception as e:
             LOGGER.error(f'{e}. If SignatureDef error. Export your model with the official onnx2tf docker')
             exit()
-            
+
         self.interpreter.allocate_tensors()  # allocate
         self.input_details = self.interpreter.get_input_details()  # inputs
         self.output_details = self.interpreter.get_output_details()  # outputs
         self.current_allocated_batch_size = self.input_details[0]['shape'][0]
 
-    def forward(self, im_batch):
+    def forward(self, im_batch: Any) -> np.ndarray:
+        """
+        Runs forward pass for the given image batch through the TFLite model.
+
+        Args:
+            im_batch (Any): Input image batch tensor.
+
+        Returns:
+            np.ndarray: Output features from the TFLite model.
+        """
         im_batch = im_batch.cpu().numpy()
         # Extract batch size from im_batch
         batch_size = im_batch.shape[0]
