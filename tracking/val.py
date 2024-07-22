7f0eeed69d3cb327ec0d52ebef93c246d5a2079b
Mikel Broström <mikel.brostrom@cevt.se>
2024-07-22 16:03:30 +0200

    only main metrics output

--- tracking/val.py
+++ tracking/val.py
@@ -176,7 +176,7 @@ def run_trackeval(opt):
     hota_mota_idf1 = parse_mot_results(trackeval_results)
     if opt.verbose:
         print(trackeval_results)
-    print(hota_mota_idf1)
+    print(json.dumps(hota_mota_idf1))
     return json.dumps(hota_mota_idf1)
 
 if __name__ == "__main__":
