23c9c3d98f317d8c34c5c6dfa56599d42d82fea9
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-08-16 17:47:42 +0200

    correct mot results frame indexing

--- tracking/utils.py
+++ tracking/utils.py
@@ -211,7 +211,7 @@ def convert_to_mot_format(results: Union[Results, np.ndarray], frame_idx: int) -
         if isinstance(results, np.ndarray):
             # Convert numpy array results to MOT format
             tlwh = ops.xyxy2ltwh(results[:, 0:4])
-            frame_idx_column = np.full((results.shape[0], 1), frame_idx + 1, dtype=np.int32)
+            frame_idx_column = np.full((results.shape[0], 1), frame_idx, dtype=np.int32)
             mot_results = np.column_stack((
                 frame_idx_column, # frame index
                 results[:, 4].astype(np.int32),  # track id
