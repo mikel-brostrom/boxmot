ec31d996c1cff0f16c3bdc66d4cffec946c956ca
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-09-05 09:39:06 +0200

    added max_age arg to imprassoc and botsort

--- boxmot/trackers/imprassoc/impr_assoc_tracker.py
+++ boxmot/trackers/imprassoc/impr_assoc_tracker.py
@@ -193,6 +193,7 @@ class ImprAssocTrack(BaseTracker):
         device,
         fp16,
         per_class=False,
+        max_age: int = 30,
         track_high_thresh: float = 0.6,
         track_low_thresh: float = 0.1,
         new_track_thresh: float = 0.7,
