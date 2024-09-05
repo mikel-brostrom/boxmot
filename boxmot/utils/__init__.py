d919aebc06db58d2dcecd05a7721c2fdd61aefc2
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-09-05 21:26:26 +0200

    per class decorator moved into basetracker

--- boxmot/utils/__init__.py
+++ boxmot/utils/__init__.py
@@ -1,1 +1,25 @@
 # Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
+
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[2]  # root directory
+DATA = ROOT / 'data'
+BOXMOT = ROOT / "boxmot"
+EXAMPLES = ROOT / "tracking"
+TRACKER_CONFIGS = ROOT / "boxmot" / "configs"
+WEIGHTS = ROOT / "tracking" / "weights"
+REQUIREMENTS = ROOT / "requirements.txt"
+
+NUM_THREADS = min(8, max(1, os.cpu_count() - 1))  # number of BoxMOT multiprocessing threads
+
+
+# global logger
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stderr, colorize=True, level="INFO")
