a1de5a4a40acad8d1641417cec90fb9415a2121a
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2024-10-01 10:19:46 +0200

    update imported tracker name

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,8 @@ from boxmot import (
     StrongSort, BotSort, DeepOcSort, OcSort, ByteTrack, ImprAssocTrack, get_tracker_config, create_tracker,
 )
 
-MOTION_N_APPEARANCE_TRACKING_METHODS = ['botsort', 'deepocsort', 'strongsort', 'imprassoc']
-MOTION_ONLY_TRACKING_METHODS = ['ocsort', 'bytetrack']
+MOTION_N_APPEARANCE_TRACKING_NAMES = ['botsort', 'deepocsort', 'strongsort', 'imprassoc']
+MOTION_ONLY_TRACKING_NAMES = ['ocsort', 'bytetrack']
 
 MOTION_N_APPEARANCE_TRACKING_METHODS=[StrongSort, BotSort, DeepOcSort, ImprAssocTrack]
 MOTION_ONLY_TRACKING_METHODS=[OcSort, ByteTrack]
