7e17ce444fd3ea5a144553c961899f7ee4f14143
LilBabines <augusteverdier@orange.fr>
2024-12-24 15:14:27 +0100

    adapt predict and get_state

--- boxmot/trackers/ocsort/ocsort.py
+++ boxmot/trackers/ocsort/ocsort.py
@@ -303,22 +303,23 @@ class KalmanBoxTrackerOBB(object):
         """
         Advances the state vector and returns the predicted bounding box estimate.
         """
-        if (self.kf.x[6] + self.kf.x[2]) <= 0:
-            self.kf.x[6] *= 0.0
-
+        if (self.kf.x[7] + self.kf.x[2]) <= 0: # Negative width
+            self.kf.x[7] *= 0.0
+        if (self.kf.x[8] + self.kf.x[3]) <= 0: # Negative Height
+            self.kf.x[8] *= 0.0
         self.kf.predict()
         self.age += 1
         if self.time_since_update > 0:
             self.hit_streak = 0
         self.time_since_update += 1
-        self.history.append(convert_x_to_bbox(self.kf.x))
+        self.history.append(self.kf.x)
         return self.history[-1]
 
     def get_state(self):
         """
         Returns the current bounding box estimate.
         """
-        return convert_x_to_bbox(self.kf.x)
+        return self.kf.x
 
 
 class OcSort(BaseTracker):
