c9a10045f04a9b4403282c41d85424129f5a45b1
LilBabines <augusteverdier@orange.fr>
2024-12-24 13:02:15 +0100

    add is_obb to  Basetracker attribute

--- boxmot/trackers/basetracker.py
+++ boxmot/trackers/basetracker.py
@@ -43,7 +43,8 @@ class BaseTracker(ABC):
         self.iou_threshold = iou_threshold
         self.last_emb_size = None
         self.asso_func_name = asso_func+"_obb" if is_obb else asso_func
-
+        self.is_obb = is_obb
+        
         self.frame_count = 0
         self.active_tracks = []  # This might be handled differently in derived classes
         self.per_class_active_tracks = None
