a268b11d1eeb12f4e89aa66990df7d496f3bfff1
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2025-03-28 18:27:46 +0100

    downgrade pkg

--- tracking/detectors/__init__.py
+++ tracking/detectors/__init__.py
@@ -47,7 +47,7 @@ def get_yolo_inferer(yolo_model):
         except (ImportError, AssertionError, AttributeError):
             checker.check_packages(('rfdetr',), cmds='--no-dependencies')  # needed dependency
             checker.check_packages(('pydantic',))  # needed dependency
-            checker.check_packages(('wandb==0.17.7',))  # needed dependency
+            checker.check_packages(('wandb',))  # needed dependency
         from .rfdetr import RFDETRStrategy
         return RFDETRStrategy
     elif 'yolo_nas' in str(yolo_model):
