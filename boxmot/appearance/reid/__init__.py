a14906e64d50c1d4fca21ecdc624cf3eddbc9a6d
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2025-04-10 11:04:50 +0200

    refactor reid

---
+++ boxmot/appearance/reid/__init__.py
@@ -0,0 +1,16 @@
+# Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
+
+import pandas as pd
+
+
+def export_formats():
+    # yolo tracking export formats
+    x = [
+        ["PyTorch", "-", ".pt", True, True],
+        ["TorchScript", "torchscript", ".torchscript", True, True],
+        ["ONNX", "onnx", ".onnx", True, True],
+        ["OpenVINO", "openvino", "_openvino_model", True, False],
+        ["TensorRT", "engine", ".engine", False, True],
+        ["TensorFlow Lite", "tflite", ".tflite", True, False],
+    ]
+    return pd.DataFrame(x, columns=["Format", "Argument", "Suffix", "CPU", "GPU"])
