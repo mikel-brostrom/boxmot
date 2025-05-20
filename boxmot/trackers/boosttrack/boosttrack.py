33190cfc646c683ac060cf4168e98cb03b68e492
Mikel Broström <mikel.brostrom@zeekrtech.eu>
2025-05-20 16:31:10 +0200

    check inputs

--- boxmot/trackers/boosttrack/boosttrack.py
+++ boxmot/trackers/boosttrack/boosttrack.py
@@ -189,6 +189,7 @@ class BoostTrack(BaseTracker):
                       [x1, y1, x2, y2, id, confidence, cls, det_ind]
                       (with cls and det_ind set to -1 if unused)
         """
+        self.check_inputs(dets, embs, img)
         if dets is None or dets.size == 0:
             dets = np.empty((0, 6))
 
