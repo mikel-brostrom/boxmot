46fbac69f1f6812e49ad6cea89f1ec0a8b545669
Mike <yolov5.deepsort.pytorch@gmail.com>
2025-05-25 14:23:28 +0200

    cli (#1983)

--- tracking/track.py
+++ tracking/track.py
@@ -1,3 +1,4 @@
+
 # Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
 
 import argparse
@@ -65,7 +66,7 @@ def plot_trajectories(predictor):
 
 
 @torch.no_grad()
-def run(args):
+def main(args):
     if args.imgsz is None:
         args.imgsz = default_imgsz(args.yolo_model)
     yolo = YOLO(
@@ -126,65 +127,5 @@ def run(args):
         pass
 
 
-def parse_opt():
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo-model', type=Path, default=WEIGHTS / 'yolov8n',
-                        help='yolo model path')
-    parser.add_argument('--reid-model', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt',
-                        help='reid model path')
-    parser.add_argument('--tracking-method', type=str, default='deepocsort',
-                        help='deepocsort, botsort, strongsort, ocsort, bytetrack, boosttrack')
-    parser.add_argument('--source', type=str, default='0',
-                        help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=None,
-                        help='inference size h,w')
-    parser.add_argument('--conf', type=float, default=0.5,
-                        help='confidence threshold')
-    parser.add_argument('--iou', type=float, default=0.7,
-                        help='intersection over union (IoU) threshold for NMS')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show', action='store_true',
-                        help='display tracking video results')
-    parser.add_argument('--save', action='store_true',
-                        help='save video tracking results')
-    # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int,
-                        help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--project', default=ROOT / 'runs' / 'track',
-                        help='save results to project/name')
-    parser.add_argument('--name', default='exp',
-                        help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true',
-                        help='existing project/name ok, do not increment')
-    parser.add_argument('--half', action='store_true',
-                        help='use FP16 half-precision inference')
-    parser.add_argument('--vid-stride', type=int, default=1,
-                        help='video frame-rate stride')
-    parser.add_argument('--show-labels', action='store_false',
-                        help='either show all or only bboxes')
-    parser.add_argument('--show-conf', action='store_false',
-                        help='hide confidences when show')
-    parser.add_argument('--show-trajectories', action='store_true',
-                        help='show confidences')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save tracking results in a txt file')
-    parser.add_argument('--save-crop', action='store_true',
-                        help='save each crop to its respective id folder')
-    parser.add_argument('--line-width', default=None, type=int,
-                        help='The line width of the bounding boxes. If None, it is scaled to the image size.')
-    parser.add_argument('--per-class', default=False, action='store_true',
-                        help='not mix up classes when tracking')
-    parser.add_argument('--verbose', default=True, action='store_true',
-                        help='print results per frame')
-    parser.add_argument('--agnostic-nms', default=False, action='store_true',
-                        help='class-agnostic NMS')
-
-    opt = parser.parse_args()
-    return opt
-
-
 if __name__ == "__main__":
-    opt = parse_opt()
-    run(opt)
+    main()
