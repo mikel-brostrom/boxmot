1658049443d2eeb8ab5a2259c8667f5ad2c24e7d
Mikel Broström <mikel.brostrom@zeekrtech.eu>
2025-05-25 13:12:30 +0200

    delete args and argparse

--- tracking/cli.py
+++ tracking/cli.py
@@ -13,66 +13,73 @@ def main():
     eval_parent.add_argument('--reid-model', nargs='+', type=Path,
                             default=[WEIGHTS/'osnet_x0_25_msmt17.pt'], help='…')
 
-    parser = argparse.ArgumentParser(prog='boxmot_cli', conflict_handler='resolve',)
+    common_parser = argparse.ArgumentParser(prog='boxmot_cli', conflict_handler='resolve',)
     # common args
-    parser.add_argument('--yolo-model', type=Path, default=WEIGHTS / 'yolov8n',
+    common_parser.add_argument('--yolo-model', type=Path, default=WEIGHTS / 'yolov8n',
                         help='yolo model path')
-    parser.add_argument('--reid-model', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt',
+    common_parser.add_argument('--reid-model', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt',
                         help='reid model path')
-    parser.add_argument('--yolo-model', type=Path, default=WEIGHTS / 'yolov8n',
+    common_parser.add_argument('--yolo-model', type=Path, default=WEIGHTS / 'yolov8n',
                         help='Path to YOLO weights')
-    parser.add_argument('--reid-model', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt',
+    common_parser.add_argument('--reid-model', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt',
                         help='Path to re-identification model')
-    parser.add_argument('--source', type=str, default="./tracking/val_utils/data/MOT17-50/train", help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img-size', nargs='+', type=int, default=None, help='inference size h,w')
-    parser.add_argument('--fps', type=int, default=None, help='video frame-rate')
-    parser.add_argument('--conf', type=float, default=0.01, help='min confidence threshold')
-    parser.add_argument('--iou', type=float, default=0.7, help='intersection over union (IoU) threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--classes', nargs='+', type=int, default=0, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--project', default=ROOT / 'runs', type=Path, help='save results to project/name')
-    parser.add_argument('--name', default='', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', default=True, help='existing project/name ok, do not increment')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    parser.add_argument('--ci', action='store_true', help='Automatically reuse existing due to no UI in CI')
-    parser.add_argument('--tracking-method', type=str, default='deepocsort', help='deepocsort, botsort, strongsort, ocsort, bytetrack, boosttrack')
-    parser.add_argument('--dets-file-path', type=Path, help='path to detections file')
-    parser.add_argument('--embs-file-path', type=Path, help='path to embeddings file')
-    parser.add_argument('--exp-folder-path', type=Path, help='path to experiment folder')
-    parser.add_argument('--verbose', action='store_true', help='print results')
-    parser.add_argument('--agnostic-nms', default=False, action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--gsi', action='store_true', help='apply Gaussian smooth interpolation postprocessing')
-    parser.add_argument('--n-trials', type=int, default=4, help='nr of trials for evolution')
-    parser.add_argument('--objectives', type=str, nargs='+', default=["HOTA", "MOTA", "IDF1"], help='set of objective metrics: HOTA,MOTA,IDF1')
-    parser.add_argument('--val-tools-path', type=Path, default=EXAMPLES / 'val_utils', help='path to store trackeval repo in')
-    parser.add_argument('--split-dataset', action='store_true', help='Use the second half of the dataset')
-    parser.add_argument('--show', action='store_true', help='Display tracking in a window')
-    parser.add_argument('--show-labels', action='store_false',
+    common_parser.add_argument('--source', type=str, default="./tracking/val_utils/data/MOT17-50/train", help='file/dir/URL/glob, 0 for webcam')
+    common_parser.add_argument('--imgsz', '--img-size', nargs='+', type=int, default=None, help='inference size h,w')
+    common_parser.add_argument('--fps', type=int, default=None, help='video frame-rate')
+    common_parser.add_argument('--conf', type=float, default=0.01, help='min confidence threshold')
+    common_parser.add_argument('--iou', type=float, default=0.7, help='intersection over union (IoU) threshold for NMS')
+    common_parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    common_parser.add_argument('--classes', nargs='+', type=int, default=0, help='filter by class: --classes 0, or --classes 0 2 3')
+    common_parser.add_argument('--project', default=ROOT / 'runs', type=Path, help='save results to project/name')
+    common_parser.add_argument('--name', default='', help='save results to project/name')
+    common_parser.add_argument('--exist-ok', action='store_true', default=True, help='existing project/name ok, do not increment')
+    common_parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    common_parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
+    common_parser.add_argument('--ci', action='store_true', help='Automatically reuse existing due to no UI in CI')
+    common_parser.add_argument('--tracking-method', type=str, default='deepocsort', help='deepocsort, botsort, strongsort, ocsort, bytetrack, boosttrack')
+    common_parser.add_argument('--dets-file-path', type=Path, help='path to detections file')
+    common_parser.add_argument('--embs-file-path', type=Path, help='path to embeddings file')
+    common_parser.add_argument('--exp-folder-path', type=Path, help='path to experiment folder')
+    common_parser.add_argument('--verbose', action='store_true', help='print results')
+    common_parser.add_argument('--agnostic-nms', default=False, action='store_true', help='class-agnostic NMS')
+    common_parser.add_argument('--gsi', action='store_true', help='apply Gaussian smooth interpolation postprocessing')
+    common_parser.add_argument('--n-trials', type=int, default=4, help='nr of trials for evolution')
+    common_parser.add_argument('--objectives', type=str, nargs='+', default=["HOTA", "MOTA", "IDF1"], help='set of objective metrics: HOTA,MOTA,IDF1')
+    common_parser.add_argument('--val-tools-path', type=Path, default=EXAMPLES / 'val_utils', help='path to store trackeval repo in')
+    common_parser.add_argument('--split-dataset', action='store_true', help='Use the second half of the dataset')
+    common_parser.add_argument('--show', action='store_true', help='Display tracking in a window')
+    common_parser.add_argument('--show-labels', action='store_false',
                         help='Hide detection labels')
-    parser.add_argument('--show-conf', action='store_false',
+    common_parser.add_argument('--show-conf', action='store_false',
                         help='Hide detection confidences')
-    parser.add_argument('--show-trajectories', action='store_true',
+    common_parser.add_argument('--show-trajectories', action='store_true',
                         help='Overlay object trajectories')
-    parser.add_argument('--save-txt', action='store_true',
+    common_parser.add_argument('--save-txt', action='store_true',
                         help='Save tracking output to text')
-    parser.add_argument('--save-crop', action='store_true',
+    common_parser.add_argument('--save-crop', action='store_true',
                         help='Save cropped detections')
-    parser.add_argument('--save', action='store_true',
+    common_parser.add_argument('--save', action='store_true',
                         help='Save tracking results')
-    parser.add_argument('--line-width', type=int, default=None,
+    common_parser.add_argument('--line-width', type=int, default=None,
                         help='Bounding box line width')
-    parser.add_argument('--per-class', action='store_true',
+    common_parser.add_argument('--per-class', action='store_true',
                         help='Track each class separately')
+    
+    parser = argparse.ArgumentParser(
+        prog='boxmot_cli',
+        parents=[common_parser],
+        conflict_handler='resolve',
+    )
+    sub = parser.add_subparsers(dest='command', required=True)
 
         # sub-commands
-    sub = parser.add_subparsers(dest='command', required=True)
-    sub.add_parser('track')
-    sub.add_parser('generate-dets-embs', parents=[eval_parent])
-    sub.add_parser('generate-mot-results', parents=[eval_parent])
-    sub.add_parser('eval', parents=[eval_parent], conflict_handler='resolve')
-    sub.add_parser('tune', parents=[eval_parent], conflict_handler='resolve')
-    sub.add_parser('all')
+    # every sub-command now gets all of the common options:
+    sub.add_parser('track',            parents=[common_parser])
+    sub.add_parser('generate-dets-embs',  parents=[common_parser, eval_parent], conflict_handler='resolve')
+    sub.add_parser('generate-mot-results', parents=[common_parser, eval_parent], conflict_handler='resolve')
+    sub.add_parser('eval',            parents=[common_parser, eval_parent], conflict_handler='resolve')
+    sub.add_parser('tune',            parents=[common_parser, eval_parent], conflict_handler='resolve')
+    sub.add_parser('all',             parents=[common_parser, eval_parent], conflict_handler='resolve')
 
     # parse and dispatch
     args = parser.parse_args()
