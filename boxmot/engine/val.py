619ccd7d74c26fa43167a510fdfa7f30385809e9
Mikel Broström <mikel.brostrom@zeekrtech.eu>
2025-06-04 14:19:54 +0200

    use ci args for benchmark generation

--- boxmot/engine/val.py
+++ boxmot/engine/val.py
@@ -384,7 +384,7 @@ def run_trackeval(opt: argparse.Namespace) -> dict:
     seq_paths, save_dir, MOT_results_folder, gt_folder = eval_setup(opt, opt.val_tools_path)
     trackeval_results = trackeval(opt, seq_paths, save_dir, MOT_results_folder, gt_folder)
     hota_mota_idf1 = parse_mot_results(trackeval_results)
-    if opt.verbose:
+    if opt.ci:
         LOGGER.info(trackeval_results)
         with open(opt.tracking_method + "_output.json", "w") as outfile:
             outfile.write(json.dumps(hota_mota_idf1))
