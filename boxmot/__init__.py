afe3947e8fb92029eb8b5f20978c760706be5097
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2025-06-05 08:59:06 +0000

    Bumping version from 13.0.2 to 13.0.3

--- boxmot/__init__.py
+++ boxmot/__init__.py
@@ -1,6 +1,6 @@
 # Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
 
-__version__ = '13.0.2'
+__version__ = '13.0.3'
 
 from boxmot.postprocessing.gsi import gsi
 from boxmot.tracker_zoo import create_tracker, get_tracker_config
