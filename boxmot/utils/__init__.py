fff205f6b2ec74cc5f66a5dc5865504dfd049c70
Mikel Broström <mikel.brostrom@zeekrtech.eu>
2025-06-26 17:27:20 +0200

    cleanup paths

--- boxmot/utils/__init__.py
+++ boxmot/utils/__init__.py
@@ -11,16 +11,18 @@ import multiprocessing as mp
 # global logger
 from loguru import logger
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[2]  # root directory
-
+ROOT = Path(__file__).resolve().parents[2]
 DATA = ROOT / "data"
-BOXMOT = ROOT / "boxmot"
 TOML = ROOT / "pyproject.toml"
-TRACKER_CONFIGS = ROOT / "boxmot" / "configs" / "trackers"
 
-EXAMPLES = BOXMOT / "engine"
-WEIGHTS = BOXMOT / "engine" / "weights"
+BOXMOT     = ROOT / "boxmot"
+CONFIGS    = BOXMOT / "configs"
+TRACKERS   = CONFIGS / "trackers"
+DATASETS   = CONFIGS / "datasets"
+
+ENGINE   = BOXMOT / "engine"
+EXAMPLES = ENGINE
+WEIGHTS  = ENGINE / "weights"
 
 NUM_THREADS = min(8, max(1, os.cpu_count() - 1))
 
