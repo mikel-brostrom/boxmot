031b742f8a1beca82175e80a11b093c88c180f9f
Mikel Broström <mikel.brostrom@zeekrtech.eu>
2025-07-23 17:34:01 +0200

    fix aspect ratio

--- tests/unit/test_trackers.py
+++ tests/unit/test_trackers.py
@@ -131,7 +131,7 @@ def test_per_class_tracker_output_size(tracker_type):
 
     rgb = np.random.randint(255, size=(640, 640, 3), dtype=np.uint8)
     det = np.array([[144, 212, 578, 480, 0.95, 0],
-                    [425, 281, 576, 472, 0.95, 65]])
+                    [400, 281, 576, 472, 0.95, 65]])
     embs = np.random.random(size=(2, 512))
 
     _ = tracker.update(det, rgb, embs)
