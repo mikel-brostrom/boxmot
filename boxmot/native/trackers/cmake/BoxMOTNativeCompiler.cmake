d1c39ecaf91d7be8ce0923878ad67a913e9ce7d2
mikel-brostrom <yolov5.deepsort.pytorch@gmail.com>
2026-04-30 12:39:22 +0200

    cleanup

--- boxmot/native/trackers/cmake/BoxMOTNativeCompiler.cmake
+++
@@ -1,5 +0,0 @@
-# Backwards-compatibility shim. Retained so external callers that include
-# this file directly continue to work. New code should
-# ``include(BoxMOTNative)`` instead, which exposes ``boxmot_add_native_tracker``
-# and the up-to-date warning helper.
-include(${CMAKE_CURRENT_LIST_DIR}/BoxMOTNative.cmake)
